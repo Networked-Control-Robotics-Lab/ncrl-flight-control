7f77ee3cf2ed242450a535ef8cb08ecd052ea299
Tei Seibun <teiseibun.tw@gmail.com>
2019-10-14 16:26:16 +0800

    fc: implement sbus parser

--- src/driver/periph/uart.c
+++ src/driver/periph/uart.c
@@ -307,9 +307,41 @@ void uart6_puts(char *s, int size)
 	while(DMA_GetFlagStatus(DMA2_Stream6, DMA_FLAG_TCIF6) == RESET);
 }
 
+
+uint16_t rc_val[15];
+
+void parse_sbus(uint8_t *raw_buff, uint16_t *rc_val)
+{
+	/* parse channel 1 to 8 */
+	rc_val[0] = ((raw_buff[1] | raw_buff[2] << 8) & 0x07ff);
+	rc_val[1] = ((raw_buff[2] >> 3 | raw_buff[3] << 5) & 0x07ff);
+	rc_val[2] = ((raw_buff[3] >> 6 | raw_buff[4] << 2 | raw_buff[5] << 10) & 0x07ff);
+	rc_val[3] = ((raw_buff[5] >> 1 | raw_buff[6] << 7) & 0x07ff);
+	rc_val[4] = ((raw_buff[6] >> 4 | raw_buff[7] << 4) & 0x07ff);
+	rc_val[5] = ((raw_buff[7] >> 7 | raw_buff[8] << 1 | raw_buff[9] << 9) & 0x07ff);
+	rc_val[6] = ((raw_buff[9] >> 2 | raw_buff[10] << 6) & 0x07ff);
+	rc_val[7] = ((raw_buff[10] >> 5 | raw_buff[11] << 3) & 0x07ff);
+
+	/* channel 9 ~ channel 16 haven't yet been validated, check the data before using them */
+	//rc_val[8] = ((raw_buff[12] | raw_buff[13] << 8) & 0x07ff);
+	//rc_val[9] = ((raw_buff[13] >> 3 | raw_buff[14] << 5) & 0x07ff);
+	//rc_val[10] = ((raw_buff[14] >> 6 | raw_buff[15] << 2 | raw_buff[16] << 10) & 0x07ff);
+	//rc_val[11] = ((raw_buff[16] >> 1 | raw_buff[17] << 7) & 0x07ff);
+	//rc_val[12] = ((raw_buff[17] >> 4 | raw_buff[18] << 4) & 0x07ff);
+	//rc_val[13] = ((raw_buff[18] >> 7 | raw_buff[19] << 1 | raw_buff[20] << 9) & 0x07ff);
+	//rc_val[14] = ((raw_buff[20] >> 2 | raw_buff[21] << 6) & 0x07ff);
+	//rc_val[15] = ((raw_buff[21] >> 5 | raw_buff[22] << 3) & 0x07ff);
+
+	/* debug message */
+	char s[100] = {0};
+	sprintf(s, "ch1:%d, ch2:%d ch3:%d, ch4:%d, ch6:%d\n\r",
+	        rc_val[0], rc_val[1], rc_val[2], rc_val[3], rc_val[5]);
+	uart3_puts(s, strlen(s));
+}
+
 uint8_t c;
 int sbus_cnt = 0;
-uint8_t sbus_buffer[25] = {0};
+uint8_t sbus_buff[25] = {0};
 
 void UART4_IRQHandler()
 {
@@ -321,20 +353,31 @@ void UART4_IRQHandler()
 			sbus_cnt = 0;
 		}
 
-		sbus_buffer[sbus_cnt] = c;
+		sbus_buff[sbus_cnt] = c;
 		sbus_cnt++;
 
+#if 1
+		if(sbus_cnt == 25) {
+			parse_sbus((uint8_t *)sbus_buff, (uint16_t *)rc_val);
+		}
+#endif
+
+#if 0
 		/* debug message */
 		if(sbus_cnt == 25) {
 			int i;
 			for(i = 0; i < 25; i++) {
 				char s[10] = {0};
-				sprintf(s, "%d,", sbus_buffer[i]);
+				sprintf(s, "%d,", sbus_buff[i]);
 				uart3_puts(s, strlen(s));
 			}
-			sbus_cnt = 0;
 			uart_putc(USART3, '\n');
 			uart_putc(USART3, '\r');
+		}
+#endif
+
+		if(sbus_cnt == 25) {
+			sbus_cnt = 0;
 		}
 	}
 }
