dd949bc0e34583c4f4a2852114a20de9eb03d038
Tei Seibun <teiseibun.tw@gmail.com>
2019-10-17 22:44:25 +0800

    fc: rewrite blocked_delay_ms()

--- src/common/delay.c
+++ src/common/delay.c
@@ -5,20 +5,9 @@
 
 void blocked_delay_ms(uint32_t ms)
 {
-	ms *= STM32_CLOCK_HZ / 1000 / STM32_CYCLES_PER_LOOP;
-
-	asm volatile(" mov r0, %[ms] \n\t"
-	             "1: subs r0, #1 \n\t"
-	             " bhi 1b \n\t"
-	             :
-	             : [ms] "r" (ms)
-	             : "r0");
-
-#if 0
-	uint32_t cnt;
-	for(cnt = ms; cnt > 0; cnt++) {
-		uint32_t tweaked_delay = 1000000;
+	volatile uint32_t cnt;
+	for(cnt = ms; cnt > 0; cnt--) {
+		volatile uint32_t tweaked_delay = 22500U;
 		while(tweaked_delay--);
 	}
-#endif
 }
