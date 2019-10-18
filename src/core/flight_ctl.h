2f941f43e01d87fc83fb27cd2925dd6e0573e1f4
Tei Seibun <teiseibun.tw@gmail.com>
2019-10-18 12:24:51 +0800

    fc: rearrange controller code to one file

--- src/core/flight_ctl.h
+++ src/core/flight_ctl.h
@@ -1,8 +1,31 @@
 #ifndef __FLIGHT_CTL_H__
 #define __FLIGHT_CTL_H__
 
-void task_flight_ctl(void *param);
+typedef struct {
+	float kp;
+	float ki;
+	float kd;
+	float p_final;
+	float i_final;
+	float d_final;
+	float error_current;
+	float error_last;
+	float error_integral;
+	float error_derivative;
+	float output;
+	float output_max;
+	float output_min;
+} pid_control_t;
 
+void attitude_pd_control(pid_control_t *pid, float ahrs_attitude,
+                         float setpoint_attitude, float angular_velocity);
+void yaw_rate_p_control(pid_control_t *pid, float setpoint_yaw_rate,
+                        float angular_velocity);
+
+void motor_control(volatile float throttle_percentage, float roll_ctrl_precentage,
+                   float pitch_ctrl_precentage, float yaw_ctrl_precentage);
+
+void task_flight_ctl(void *param);
 void flight_ctl_semaphore_handler(void);
 
 #endif
