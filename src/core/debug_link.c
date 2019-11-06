443e9b0c185e25f8004bb798440667a067d5ea18
Tei Seibun <teiseibun.tw@gmail.com>
2019-11-06 12:48:34 +0800

    fc: replace blocked delay with vTaskDelay

--- src/core/debug_link.c
+++ src/core/debug_link.c
@@ -197,15 +197,13 @@ void send_pid_debug(void)
 void task_debug_link(void *param)
 {
 	while(1) {
-		blocked_delay_ms(20);
-
-		//send_imu_message();
+		send_imu_message();
 		//send_attitude_euler_message();
 		//send_attitude_quaternion_message();
-		send_attitude_imu_message();
+		//send_attitude_imu_message();
 		//send_ekf_message();
 		//send_pid_debug();
 		//send_motor_message();
-		taskYIELD();
+		vTaskDelay(200);
 	}
 }
