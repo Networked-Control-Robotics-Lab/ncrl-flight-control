37ee3c943281fdb7c8c004d92e99f2ac5ca61287
Tei Seibun <teiseibun.tw@gmail.com>
2019-11-20 19:59:31 +0800

    optitrack: add optitrack client (python)

---
+++ optitrack/PythonSample.py
@@ -0,0 +1,21 @@
+﻿from NatNetClient import NatNetClient
+
+# This is a callback function that gets connected to the NatNet client and called once per mocap frame.
+def receiveNewFrame( frameNumber, markerSetCount, unlabeledMarkersCount, rigidBodyCount, skeletonCount,
+                    labeledMarkerCount, timecode, timecodeSub, timestamp, isRecording, trackedModelsChanged ):
+    print( "Received frame", frameNumber )
+
+# This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
+def receiveRigidBodyFrame( id, position, rotation ):
+    print( "Received frame for rigid body", id )
+
+# This will create a new NatNet client
+streamingClient = NatNetClient()
+
+# Configure the streaming client to call our rigid body handler on the emulator to send data out.
+streamingClient.newFrameListener = receiveNewFrame
+streamingClient.rigidBodyListener = receiveRigidBodyFrame
+
+# Start up the streaming client now that the callbacks are set up.
+# This will run perpetually, and operate on a separate thread.
+streamingClient.run()
