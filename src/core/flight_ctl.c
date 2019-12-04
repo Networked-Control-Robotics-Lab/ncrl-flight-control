35c2c3887ffd01092c4fbf4015d898613743ac15
Tei Seibun <teiseibun.tw@gmail.com>
2019-12-04 11:03:44 +0800

    fc: final tune of position controller

--- src/core/flight_ctl.c
+++ src/core/flight_ctl.c
@@ -71,15 +71,15 @@ void pid_controller_init(void)
 	pid_yaw.output_max = 35.0f;
 
 	/* positon and velocity controllers */
-	pid_pos_x.kp = 0.04f;
-	pid_pos_x.ki = 0.001f;
-	pid_pos_x.kd = 0.054f;
+	pid_pos_x.kp = 0.05f;
+	pid_pos_x.ki = 0.006f;
+	pid_pos_x.kd = 0.067f;
 	pid_pos_x.output_min = -15.0f;
 	pid_pos_x.output_max = +15.0f;
 
-	pid_pos_y.kp = 0.04f;
-	pid_pos_y.ki = 0.001f;
-	pid_pos_y.kd = 0.054f;
+	pid_pos_y.kp = 0.05f;
+	pid_pos_y.ki = 0.006f;
+	pid_pos_y.kd = 0.067f;
 	pid_pos_y.output_min = -15.0f;
 	pid_pos_y.output_max = +15.0f;
 
@@ -87,7 +87,7 @@ void pid_controller_init(void)
 	pid_alt.ki = 0.09f;
 	pid_alt.kd = 0.0f;
 
-	pid_alt_vel.kp = 0.08f;
+	pid_alt_vel.kp = 0.09f;
 	pid_alt_vel.ki = 0.0f;
 	pid_alt_vel.kd = 0.0f;
 	pid_alt_vel.output_min = -100.0f;
