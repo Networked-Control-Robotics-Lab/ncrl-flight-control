2acc362a67dc447cdbc980f4745c1fb62080551a
Tei Seibun <teiseibun.tw@gmail.com>
2019-12-31 17:00:24 +0800

    fc: boind thrust curve

--- src/core/controllers/motor_thrust.c
+++ src/core/controllers/motor_thrust.c
@@ -1,9 +1,14 @@
 #include <stdint.h>
+#include <bound.h>
 
 /* using polynomial functions for thrust curve line fitting */
 
+#define THRUST_MAX 914.0 //[g]
+
 float convert_motor_cmd_to_thrust(float percentage)
 {
+	bound_float(&percentage, 100.0f, 0.0f);
+
 	float x = percentage;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
@@ -12,6 +17,8 @@ float convert_motor_cmd_to_thrust(float percentage)
 
 float convert_motor_thrust_to_cmd(float thrust)
 {
+	bound_float(&thrust, THRUST_MAX, 0.0f);
+
 	float x = thrust;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
