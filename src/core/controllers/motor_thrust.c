3c5f0d7167d8d77940d95fd368c3fc0ae3c48873
Tei Seibun <teiseibun.tw@gmail.com>
2020-01-02 01:04:44 +0800

    fc: fix thrust curve's coefficients

--- src/core/controllers/motor_thrust.c
+++ src/core/controllers/motor_thrust.c
@@ -12,7 +12,7 @@ float convert_motor_cmd_to_thrust(float percentage)
 	float x = percentage;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
-	return -2323.0f * x_cubed + 3504.0f * x_squared + x;
+	return (-2323.0f) * x_cubed + (3504.0f) * x_squared + (-260.0f) * x;
 }
 
 float convert_motor_thrust_to_cmd(float thrust)
@@ -22,5 +22,5 @@ float convert_motor_thrust_to_cmd(float thrust)
 	float x = thrust;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
-	return 0.000000027 * x_cubed + 0.0000038811 * x_squared + 0.0023461340 * x;
+	return (0.0000000027) * x_cubed + (-0.0000038811) * x_squared + (0.0023461340) * x;
 }
