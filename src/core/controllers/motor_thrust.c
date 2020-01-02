0fa425fe39222d4d0583d62eaeaecfa515bf16d8
Tei Seibun <teiseibun.tw@gmail.com>
2020-01-02 11:45:21 +0800

    fc: bound motor thrust fit line

--- src/core/controllers/motor_thrust.c
+++ src/core/controllers/motor_thrust.c
@@ -5,6 +5,7 @@
 
 #define THRUST_MAX 914.0 //[g]
 
+//FIXME: Current range is 0~0.1 rather than 0%~100%
 float convert_motor_cmd_to_thrust(float percentage)
 {
 	bound_float(&percentage, 100.0f, 0.0f);
@@ -12,9 +13,14 @@ float convert_motor_cmd_to_thrust(float percentage)
 	float x = percentage;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
-	return (-2323.0f) * x_cubed + (3504.0f) * x_squared + (-260.0f) * x;
+	float thrust = (-2323.0f) * x_cubed + (3504.0f) * x_squared + (-260.0f) * x;
+
+	bound_float(&thrust, THRUST_MAX, 0);
+
+	return thrust;
 }
 
+//FIXME: Current range is 0~0.1 rather than 0%~100%
 float convert_motor_thrust_to_cmd(float thrust)
 {
 	bound_float(&thrust, THRUST_MAX, 0.0f);
@@ -22,5 +28,9 @@ float convert_motor_thrust_to_cmd(float thrust)
 	float x = thrust;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
-	return (0.0000000027) * x_cubed + (-0.0000038811) * x_squared + (0.0023461340) * x;
+
+	float percentage = (0.0000000027) * x_cubed + (-0.0000038811) * x_squared + (0.0023461340) * x;
+	bound_float(&percentage, 1.0f, 0.0f);
+
+	return percentage;
 }
