f9fc63999ee327b9c5908df635823091ad35c21a
Tei Seibun <teiseibun.tw@gmail.com>
2020-02-08 02:05:16 +0800

    ahrs: rearrange the header file

--- src/core/estimators/madgwick_ahrs.h
+++ src/core/estimators/madgwick_ahrs.h
@@ -1,5 +1,5 @@
-#ifndef Madgwick_estimator_H
-#define Madgwick_estimator_H
+#ifndef __madgwick_ahrs_h__
+#define __madgwick_ahrs_h__
 
 #include <stdint.h>
 #include "math.h"
@@ -12,7 +12,7 @@ typedef struct _madgwick_t {
 
 void madgwick_init(madgwick_t* madgwick, float sample_rate, float beta);
 void madgwick_imu_ahrs(madgwick_t* Madgwick, float ax, float ay, float az, float gx, float gy, float gz);
-void Madgwick_MARG_AHRS(madgwick_t* Madgwick, float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
+void madgwick_margs_ahrs(madgwick_t* Madgwick, float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
 
 #endif
 
