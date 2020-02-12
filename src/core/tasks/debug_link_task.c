0a796b59294edfa0c7c6664115db1b72d3cb0a10
Tei Seibun <teiseibun.tw@gmail.com>
2020-02-12 17:29:47 +0800

    serial_plot: enable only one kind of debug link message

--- src/core/tasks/debug_link_task.c
+++ src/core/tasks/debug_link_task.c
@@ -15,19 +15,21 @@ void task_debug_link(void *param)
 	float update_rate = 50;
 	float delay_time_ms = (1.0f / update_rate) * 1000.0f;
 
+	/* only one kind of debug link message can be sent by one time,
+	 * choose the one by uncomment it */
 	while(1) {
-		send_imu_debug_message(&payload);
-		send_attitude_euler_debug_message(&payload);
+		//send_imu_debug_message(&payload);
+		//send_attitude_euler_debug_message(&payload);
 		send_attitude_quaternion_debug_message(&payload);
-		send_attitude_imu_debug_message(&payload);
-		send_pid_debug_message(&payload);
-		send_motor_debug_message(&payload);
-		send_optitrack_position_debug_message(&payload);
-		send_optitrack_quaternion_debug_message(&payload);
-		send_optitrack_velocity_debug_message(&payload);
-		send_geometry_ctrl_debug(&payload);
-		send_uav_dynamics_debug(&payload);
-		send_free_fall_debug_message(&payload);
+		//send_attitude_imu_debug_message(&payload);
+		//send_pid_debug_message(&payload);
+		//send_motor_debug_message(&payload);
+		//send_optitrack_position_debug_message(&payload);
+		//send_optitrack_quaternion_debug_message(&payload);
+		//send_optitrack_velocity_debug_message(&payload);
+		//send_geometry_ctrl_debug(&payload);
+		//send_uav_dynamics_debug(&payload);
+		//send_free_fall_debug_message(&payload);
 
 		send_onboard_data(payload.s, payload.len);
 		freertos_task_delay(delay_time_ms);
