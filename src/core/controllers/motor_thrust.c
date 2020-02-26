2447539afaf9a9bb4344cb8d7baf3fbc92e183cd
Tei Seibun <teiseibun.tw@gmail.com>
2020-02-26 11:23:28 +0800

    calibrate motor thrust curve

--- src/core/controllers/motor_thrust.c
+++ src/core/controllers/motor_thrust.c
@@ -3,7 +3,7 @@
 
 /* using polynomial functions for thrust curve line fitting */
 
-#define THRUST_MAX 914.0 //[g]
+#define THRUST_MAX 845.0 //[g]
 
 //FIXME: Current range is 0~0.1 rather than 0%~100%
 float convert_motor_cmd_to_thrust(float percentage)
@@ -13,7 +13,7 @@ float convert_motor_cmd_to_thrust(float percentage)
 	float x = percentage;
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
-	float thrust = (-2323.0f) * x_cubed + (3504.0f) * x_squared + (-260.0f) * x;
+	float thrust = (-1831.0f) * x_cubed + (2836.0f) * x_squared + (-147.0f) * x;
 
 	bound_float(&thrust, THRUST_MAX, 0);
 
@@ -29,7 +29,7 @@ float convert_motor_thrust_to_cmd(float thrust)
 	float x_squared = x * x;
 	float x_cubed = x_squared * x;
 
-	float percentage = (0.0000000027) * x_cubed + (-0.0000038811) * x_squared + (0.0023461340) * x;
+	float percentage = (0.0000000027) * x_cubed + (-0.0000037421) * x_squared + (0.0023565529) * x;
 	bound_float(&percentage, 1.0f, 0.0f);
 
 	return percentage;
