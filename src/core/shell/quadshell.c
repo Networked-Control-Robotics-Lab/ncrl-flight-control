d68eb59faad0883776f5615b7130f79cf7d34a31
Tei Seibun <teiseibun.tw@gmail.com>
2020-03-03 18:01:41 +0800

    port quadshell

---
+++ src/core/shell/quadshell.c
@@ -0,0 +1,277 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+#include "quadshell.h"
+
+/* port your own getc function here */
+static char shell_getc(void)
+{
+	char c;
+	uart3_getc(&c);
+	return c;
+}
+
+/* port your own puts function here */
+static void shell_puts(char *s)
+{
+	usart_puts(USART3, s, strlen(s));
+}
+
+/* define your own ctrl+c behavior here */
+static void shell_ctrl_c_handler(void)
+{
+	shell_puts("^C\n\r");
+}
+
+/* define your own unknown shell command behavior here */
+static void shell_unknown_cmd_handler(char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX], int param_cnt)
+{
+	char s[200];
+	sprintf(s, "unknown command: %s\n\r", param_list[0]);
+	shell_puts(s);
+}
+
+void shell_cls(void)
+{
+	shell_puts("\x1b[H\x1b[2J");
+}
+
+void shell_init_struct(struct shell_struct *shell, char *prompt_msg, char *ret_cmd)
+{
+	shell->prompt_msg = prompt_msg;
+	shell->prompt_len = strlen(shell->prompt_msg);
+
+	shell->char_cnt = 0;
+	shell->buf = ret_cmd;
+
+	shell->cursor_pos = 0;
+	memset(shell->buf, '\0', CMD_LEN_MAX);
+}
+
+static void shell_reset_struct(struct shell_struct *shell)
+{
+	shell->cursor_pos = 0;
+	shell->char_cnt = 0;
+}
+
+static void shell_remove_char(struct shell_struct *shell, int remove_pos)
+{
+	int i;
+	for(i = (remove_pos - 1); i < (shell->char_cnt); i++) {
+		shell->buf[i] = shell->buf[i + 1];
+	}
+
+	shell->buf[shell->char_cnt] = '\0';
+	shell->char_cnt--;
+	shell->cursor_pos--;
+
+	if(shell->cursor_pos > shell->char_cnt) {
+		shell->cursor_pos = shell->char_cnt;
+	}
+}
+
+static void shell_insert_char(struct shell_struct *shell, char c)
+{
+	int i;
+	for(i = shell->char_cnt; i > (shell->cursor_pos - 1); i--) {
+		shell->buf[i] = shell->buf[i - 1];
+	}
+	shell->char_cnt++;
+	shell->buf[shell->char_cnt] = '\0';
+
+	shell->buf[shell->cursor_pos] = c;
+	shell->cursor_pos++;
+}
+
+static void shell_refresh_line(struct shell_struct *shell)
+{
+	char s[PROMPT_LEN_MAX * 2];
+	sprintf(s, "\33[2K\r"   /* clear current line */
+                "%s%s\r"        /* show prompt */
+                "\033[%dC",     /* move cursor */
+                shell->prompt_msg, shell->buf, shell->prompt_len + shell->cursor_pos);
+	shell_puts(s);
+}
+
+void shell_cli(struct shell_struct *shell)
+{
+	shell_puts(shell->prompt_msg);
+
+	int c;
+	char seq[2];
+	while(1) {
+		c = shell_getc();
+
+		switch(c) {
+		case NULL_CH:
+			break;
+		case CTRL_A:
+			shell->cursor_pos = 0;
+			shell_refresh_line(shell);
+			break;
+		case CTRL_C:
+			shell_ctrl_c_handler();
+			return;
+			break;
+		case CTRL_D:
+			break;
+		case CTRL_E:
+			if(shell->char_cnt > 0) {
+				shell->cursor_pos = shell->char_cnt;
+				shell_refresh_line(shell);
+			}
+			break;
+		case CTRL_F:
+			break;
+		case CTRL_G:
+			break;
+		case CTRL_H:
+			break;
+		case TAB:
+			break;
+		case CTRL_J:
+			break;
+		case ENTER:
+			if(shell->char_cnt > 0) {
+				shell_puts("\n\r");
+				shell_reset_struct(shell);
+				return;
+			} else {
+				shell_puts("\n\r");
+				shell_puts(shell->prompt_msg);
+			}
+			break;
+		case CTRL_K:
+			break;
+		case CTRL_L:
+			break;
+		case CTRL_N:
+			break;
+		case CTRL_O:
+			break;
+		case CTRL_P:
+			break;
+		case CTRL_Q:
+			break;
+		case CTRL_R:
+			break;
+		case CTRL_S:
+			break;
+		case CTRL_T:
+			break;
+		case CTRL_U:
+			break;
+		case CTRL_W:
+			break;
+		case CTRL_X:
+			break;
+		case CTRL_Y:
+			break;
+		case CTRL_Z:
+			break;
+		case ESC_SEQ1:
+			seq[0] = shell_getc();
+			seq[1] = shell_getc();
+			if(seq[0] == ESC_SEQ2) {
+				if(seq[1] == UP_ARROW) {
+				} else if(seq[1] == DOWN_ARROW) {
+				} else if(seq[1] == RIGHT_ARROW) {
+					if(shell->cursor_pos < shell->char_cnt) {
+						shell->cursor_pos++;
+						shell_puts("\033[1C");
+					}
+				} else if(seq[1] == LEFT_ARROW) {
+					if(shell->cursor_pos > 0) {
+						shell_puts("\033[1D");
+						shell->cursor_pos--;
+					}
+				} else if(seq[1] == HOME) {
+					shell->cursor_pos = 0;
+					shell_refresh_line(shell);
+				} else if(seq[1] == END) {
+					if(shell->char_cnt > 0) {
+						shell->cursor_pos = shell->char_cnt;
+						shell_refresh_line(shell);
+					}
+				} else if(seq[1] == DELETE && shell_getc() == ESC_SEQ4) {
+					if(shell->char_cnt != 0 && shell->cursor_pos != shell->char_cnt) {
+						shell_remove_char(shell, shell->cursor_pos + 1);
+						shell->cursor_pos++;
+						shell_refresh_line(shell);
+					}
+				}
+			}				
+			break;
+		case BACKSPACE:
+			if(shell->char_cnt != 0 && shell->cursor_pos != 0) {
+				shell_remove_char(shell, shell->cursor_pos);
+				shell_refresh_line(shell);
+			}
+			break;
+		case SPACE:
+		default:
+			if(shell->char_cnt != (CMD_LEN_MAX - 1)) {
+				shell_insert_char(shell, c);
+				shell_refresh_line(shell);
+			}
+			break;
+		}
+	}
+}
+
+static void shell_split_cmd_token(char *cmd, char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX], int *param_cnt)
+{
+	int param_list_index = 0;
+	int i = 0, j = 0;
+
+	int cmd_s_len = strlen(cmd);
+
+	/* skip spaces before first parameter */
+	while(i < cmd_s_len && cmd[i] == ' ') {
+		i++;
+	}
+
+	for(; i < cmd_s_len; i++) {
+		if(cmd[i] == ' ') {
+			param_list[param_list_index][j] = '\0';
+			param_list_index++;
+			j = 0;
+
+			/* exceed maximum parameter count */
+			if(param_list_index == PARAM_LIST_SIZE_MAX) {
+				*param_cnt = param_list_index;
+				return;
+			}
+
+			/* skip spaces */
+			while(cmd[i + 1] == ' ' && i < cmd_s_len) {
+				i++;
+			}
+		} else {
+			param_list[param_list_index][j] = cmd[i];
+			j++;
+		}
+	}
+	*param_cnt = param_list_index + 1;
+}
+
+void shell_cmd_exec(char *cmd, struct cmd_list_entry *cmd_list, int list_size)
+{
+	char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX] = {0};
+	int param_cnt;
+	shell_split_cmd_token(cmd, param_list, &param_cnt);
+
+	int i;
+	for(i = 0; i < list_size; i++) {
+		if(strcmp(param_list[0], cmd_list[i].name) == 0) {
+			cmd_list[i].handler(param_list, param_cnt);
+			cmd[0] = '\0';
+			return;
+		}
+	}
+
+	shell_unknown_cmd_handler(param_list, param_cnt);
+	cmd[0] = '\0';
+}
---
+++ src/core/shell/quadshell.h
@@ -0,0 +1,71 @@
+#ifndef __QUAD_SHELL_H__
+#define __QUAD_SHELL_H__
+
+#define CMD_LEN_MAX 50
+#define PROMPT_LEN_MAX 50
+
+#define PARAM_LIST_SIZE_MAX 10
+#define PARAM_LEN_MAX 10
+
+#define SIZE_OF_SHELL_CMD_LIST(list) (sizeof(list) / sizeof(struct cmd_list_entry))
+#define DEF_SHELL_CMD(cmd_name) {.handler = shell_cmd_ ## cmd_name, .name = #cmd_name},
+
+enum {
+	NULL_CH = 0,       /* null character */
+	CTRL_A = 1,        /* ctrl + a*/
+	CTRL_C = 3,        /* ctrl + c */
+	CTRL_D = 4,        /* ctrl + d */
+	CTRL_E = 5,        /* ctrl + e */
+	CTRL_F = 6,        /* ctrl + f */
+	CTRL_G = 7,        /* ctrl + g */
+	CTRL_H = 8,        /* ctrl + h */
+	TAB = 9,           /* tab */
+	CTRL_J = 10,       /* ctrl + j */
+	CTRL_K = 11,       /* ctrl + k */
+	CTRL_L = 12,       /* ctrl + l */
+	ENTER = 13,        /* enter */
+	CTRL_N = 14,       /* ctrl + n */
+	CTRL_O = 15,       /* ctrl + o */
+	CTRL_P = 16,       /* ctrl + p */
+	CTRL_Q = 17,       /* ctrl + r */
+	CTRL_R = 18,       /* ctrl + r */
+	CTRL_S = 19,       /* ctrl + s */
+	CTRL_T = 20,       /* ctrl + t */
+	CTRL_U = 21,       /* ctrl + u */
+	CTRL_W = 23,       /* ctrl + w */
+	CTRL_X = 24,       /* ctrl + x */
+	CTRL_Y = 25,       /* ctrl + y */
+	CTRL_Z = 26,       /* ctrl + z */
+	ESC_SEQ1 = 27,     /* first byte of the vt escape sequence */
+	SPACE = 32,        /* space */
+	DELETE = 51,       /* delete, third byte of the vt escape sequence */
+	UP_ARROW = 65,     /* up arrow, third byte of the xterm escape sequence */
+	DOWN_ARROW = 66,   /* down arrow, third byte of the xterm escape sequence */
+	RIGHT_ARROW = 67,  /* right arrow, third byte of the xterm escape sequence */
+	LEFT_ARROW = 68,   /* left arrow, third byte of the xterm escape sequence */
+	END = 70,          /* end, third byte of the escape sequence */
+	HOME = 72,         /* home, third byte of the escape sequence */
+	ESC_SEQ2 = 91,     /* second byte of the escape sequence */
+	ESC_SEQ4 = 126,    /* fourth byte of the escape sequence */
+	BACKSPACE = 127,   /* backspace */
+} KEYS;
+
+struct shell_struct {
+	int cursor_pos;
+	int char_cnt;
+	int prompt_len;
+	char *buf;
+	char *prompt_msg;
+};
+
+struct cmd_list_entry {
+	void (*handler)(char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX], int param_cnt);
+	char name[PROMPT_LEN_MAX];
+};
+
+void shell_init_struct(struct shell_struct *_shell, char *prompt_msg, char *ret_cmd);
+void shell_cls(void);
+void shell_cli(struct shell_struct *_shell);
+void shell_cmd_exec(char *cmd, struct cmd_list_entry *cmd_list, int list_size);
+
+#endif
