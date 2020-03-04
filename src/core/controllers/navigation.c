928674cebc2ba3cd8bfc275d966fe438556dc536
Tei Seibun <teiseibun.tw@gmail.com>
2020-03-04 21:56:47 +0800

    nav: complete waypoint manager

--- src/core/controllers/navigation.c
+++ src/core/controllers/navigation.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include "arm_math.h"
+#include "sys_time.h"
 #include "navigation.h"
 
 #define EARTH_RADIUS 6371 //[km]
@@ -54,17 +55,119 @@ int nav_add_new_waypoint(float pos[3], float heading, float halt_time_sec)
 	}
 }
 
+int nav_clear_waypoint_list(void)
+{
+	if(nav_ptr->mode != NAV_FOLLOW_WAYPOINT &&
+	   nav_ptr->mode != NAV_WAIT_NEXT_WAYPOINT) {
+		nav_ptr->wp_num = 0;
+		nav_ptr->curr_wp = 0;
+		return WP_SET_SUCCEED;
+	} else {
+		return WP_WP_LIST_EXECUTING;
+	}
+}
+
 int nav_goto_waypoint_now(float pos[3], float heading)
 {
 	bool in_fence = nav_test_point_in_rectangular_fence(pos);
 
 	if(in_fence == true) {
+		nav_ptr->mode = NAV_HOVERING_WAYPOINT; 
 		(nav_ptr->wp_now).pos[0] = pos[0];
 		(nav_ptr->wp_now).pos[1] = pos[1];
 		(nav_ptr->wp_now).pos[2] = pos[2];
 		(nav_ptr->wp_now).heading = heading;
+		nav_ptr->curr_wp = 0; //reset waypoint list pointer
 		return WP_SET_SUCCEED;
 	} else {
 		return WP_SET_OUT_OF_FENCE;
+	}
+}
+
+int nav_halt_waypoint_mission(void)
+{
+	if(nav_ptr->mode == NAV_FOLLOW_WAYPOINT) {
+		nav_ptr->halt_flag = true;
+		return WP_SET_SUCCEED;
+	} else {
+		return WP_NO_EXECUTING_WP_LIST;
+	}
+}
+
+int nav_resume_waypoint_mission(void)
+{
+	if(nav_ptr->curr_wp != 0 && nav_ptr->curr_wp != (nav_ptr->wp_num - 1)) {
+		nav_ptr->mode = NAV_FOLLOW_WAYPOINT;
+		return WP_SET_SUCCEED;
+	} else {
+		return WP_NO_EXECUTING_WP_LIST;
+	}
+}
+
+int nav_waypoint_mission_start(void)
+{
+	if(nav_ptr->wp_num >= 1) {
+		nav_ptr->curr_wp = 0;
+		nav_ptr->mode = NAV_FOLLOW_WAYPOINT;
+		return WP_SET_SUCCEED;
+	} else {
+		return WP_WP_LIST_EMPTY;
+	}
+}
+
+void nav_waypoint_handler(float curr_pos[3])
+{
+	static float start_time = 0.0f;
+	float curr_time = 0.0f;
+
+	if(nav_ptr->mode == NAV_TRAJECTORY_FOLLOWING_MODE ||
+	   nav_ptr->mode == NAV_HOVERING_WAYPOINT) {
+		return;
+	}
+
+	/* if receive halt command */
+	if(nav_ptr->halt_flag == true) {
+		/* hovering at current position */
+		nav_ptr->wp_now.pos[0] = curr_pos[0];
+		nav_ptr->wp_now.pos[1] = curr_pos[1];
+		nav_ptr->wp_now.pos[2] = curr_pos[2];
+		nav_ptr->halt_flag = false;
+		nav_ptr->mode = NAV_HOVERING_WAYPOINT; 
+	}
+
+	if(nav_ptr->mode == NAV_WAIT_NEXT_WAYPOINT) {
+		curr_time = get_sys_time_s();
+		/* check if time is up */
+		if((curr_time - start_time) > nav_ptr->wp_list[nav_ptr->curr_wp].halt_time_sec) {
+			/* continue next waypoint if exist */
+			if(nav_ptr->curr_wp < (nav_ptr->wp_num - 1)) {
+				nav_ptr->mode = NAV_FOLLOW_WAYPOINT;
+				nav_ptr->curr_wp++;
+			} else {
+				/* there is no more waypoints, hovering at the last one's position */
+				nav_ptr->wp_now.pos[0] = nav_ptr->wp_list[nav_ptr->curr_wp].pos[0];
+				nav_ptr->wp_now.pos[1] = nav_ptr->wp_list[nav_ptr->curr_wp].pos[1];
+				nav_ptr->wp_now.pos[2] = nav_ptr->wp_list[nav_ptr->curr_wp].pos[2];
+				nav_ptr->mode = NAV_HOVERING_WAYPOINT;
+			}
+		}
+	} else if(nav_ptr->mode == NAV_FOLLOW_WAYPOINT) {
+		/* calculate 2-norm to check if enter the waypoint touch zone or not */
+		float curr_dist[3];
+		curr_dist[0] = curr_pos[0] - nav_ptr->wp_list[nav_ptr->curr_wp].pos[0];
+		curr_dist[0] *= curr_dist[0];
+		curr_dist[1] = curr_pos[1] - nav_ptr->wp_list[nav_ptr->curr_wp].pos[1];
+		curr_dist[1] *= curr_dist[1];
+		curr_dist[2] = curr_pos[2] - nav_ptr->wp_list[nav_ptr->curr_wp].pos[2];
+		curr_dist[2] *= curr_dist[2];
+
+		float accept_dist = nav_ptr->wp_list[nav_ptr->curr_wp].touch_radius;
+		accept_dist *= accept_dist;
+
+		if((curr_dist[0] + curr_dist[1] + curr_dist[2]) < accept_dist) {
+			/* start the timer */
+			start_time = get_sys_time_s();
+			nav_ptr->mode = NAV_WAIT_NEXT_WAYPOINT;
+		}
 	}
 }
--- src/core/controllers/navigation.h
+++ src/core/controllers/navigation.h
@@ -1,23 +1,35 @@
 #ifndef __NAVIGATION_H__
 #define __NAVIGATION_H__
 
+#include <stdbool.h>
+
 #define WAYPOINT_NUM_MAX 50
 
 enum {
-	NAV_POINT_TO_POINT_MODE,
-	NAV_TRAJECTORY_MODE
+	/* hovering at a waypoint */
+	NAV_HOVERING_WAYPOINT,
+	/* follow the waypoint list to fly */
+	NAV_FOLLOW_WAYPOINT,
+	/* hovering by current waypoint for some time before traveling to next one */
+	NAV_WAIT_NEXT_WAYPOINT,
+	/* follow a trajectory by given position, velocity and acceleration */
+	NAV_TRAJECTORY_FOLLOWING_MODE
 } NAV_MODE;
 
 enum {
 	WP_SET_SUCCEED,
 	WP_SET_OUT_OF_FENCE,
 	WP_SET_EXCEED_MAX_WP,
+	WP_NO_EXECUTING_WP_LIST,
+	WP_WP_LIST_EXECUTING,
+	WP_WP_LIST_EMPTY
 } WP_SET_RETVAL;
 
 struct waypoint_t {
-	float pos[3];
-	float heading;
-	float halt_time;
+	float pos[3];        //[m]
+	float heading;       //[deg]
+	float halt_time_sec; //[s]
+	float touch_radius;  //[m]
 };
 
 typedef struct {
@@ -31,7 +43,10 @@ typedef struct {
 		float height;
 	} geo_fence; /* rectangular geo-fence in enu frame */
 
-	struct waypoint_t wp_list[WAYPOINT_NUM_MAX]; //enu frame!
+	int mode;
+	bool halt_flag;
+
+	struct waypoint_t wp_list[WAYPOINT_NUM_MAX]; //enu frame
 	int curr_wp;
 	int wp_num;
 } nav_t;
@@ -39,6 +54,11 @@ typedef struct {
 void nav_init(nav_t *_nav);
 void nav_set_enu_rectangular_fence(float origin[3], float lx, float ly, float height);
 int nav_add_new_waypoint(float pos[3], float heading, float halt_time_sec);
+int nav_clear_waypoint_list(void);
 int nav_goto_waypoint_now(float pos[3], float heading);
+int nav_halt_waypoint_mission(void);
+int nav_resume_waypoint_mission(void);
+int nav_waypoint_mission_start(void);
+void nav_waypoint_handler(float curr_pos[3]);
 
 #endif
