753e9fd1050888b5413794030bf11e72bf14d226
Tei Seibun <teiseibun.tw@gmail.com>
2020-03-05 13:02:05 +0800

    add se3_math module

---
+++ src/common/se3_math.h
@@ -0,0 +1,11 @@
+#ifndef __SE3_MATH_H__
+#define __SE3_MATH_H__
+
+void euler_to_rotation_matrix(euler_t *euler, float *r, float *r_transpose);
+void quat_to_rotation_matrix(float *q, float *r, float *r_transpose);
+void vee_map_3x3(float *mat, float *vec);
+void cross_product_3x1(float *vec_a, float *vec_b, float *vec_result);
+void norm_3x1(float *vec, float *norm);
+void normalize_3x1(float *vec);
+
+#endif
--- src/core/controllers/multirotor_geometry_ctrl.c
+++ src/core/controllers/multirotor_geometry_ctrl.c
@@ -11,6 +11,7 @@
 #include "motor_thrust.h"
 #include "motor.h"
 #include "bound.h"
+#include "se3_math.h"
 #include "lpf.h"
 #include "imu.h"
 #include "ahrs.h"
@@ -151,127 +152,6 @@ void geometry_ctrl_init(void)
 	/* z-axis tracking gains */
 	kpz = 10.0f;
 	kvz = 4.0f;
-}
-
-void euler_to_rotation_matrix(euler_t *euler, float *r, float *r_transpose)
-{
-	/* check: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles */
-	/* R = Rz(psi)Ry(theta)Rx(phi)*/
-	float cos_phi = arm_cos_f32(euler->roll);
-	float cos_theta = arm_cos_f32(euler->pitch);
-	float cos_psi = arm_cos_f32(euler->yaw);
-	float sin_phi = arm_sin_f32(euler->roll);
-	float sin_theta = arm_sin_f32(euler->pitch);
-	float sin_psi = arm_sin_f32(euler->yaw);
-
-	//R
-	r[0*3 + 0] = cos_theta * cos_psi;
-	r[0*3 + 1] = (-cos_phi * sin_psi) + (sin_phi * sin_theta * cos_psi);
-	r[0*3 + 2] = (sin_phi * sin_psi) + (cos_phi * sin_theta * cos_psi);
-
-	r[1*3 + 0] = cos_theta * sin_psi;
-	r[1*3 + 1] = (cos_phi * cos_psi) + (sin_phi * sin_theta * sin_psi);
-	r[1*3 + 2] = (-sin_phi * cos_psi) + (cos_phi * sin_theta * sin_psi);
-
-	r[2*3 + 0] = -sin_theta;
-	r[2*3 + 1] = sin_phi * cos_theta;
-	r[2*3 + 2] = cos_phi * cos_theta;
-
-	//transpose(R)
-	r_transpose[0*3 + 0] = r[0*3 + 0];
-	r_transpose[1*3 + 0] = r[0*3 + 1];
-	r_transpose[2*3 + 0] = r[0*3 + 2];
-
-	r_transpose[0*3 + 1] = r[1*3 + 0];
-	r_transpose[1*3 + 1] = r[1*3 + 0];
-	r_transpose[2*3 + 1] = r[1*3 + 2];
-
-	r_transpose[0*3 + 2] = r[2*3 + 0];
-	r_transpose[1*3 + 2] = r[2*3 + 1];
-	r_transpose[2*3 + 2] = r[2*3 + 2];
-}
-
-void quat_to_rotation_matrix(float *q, float *r, float *r_transpose)
-{
-	/* check: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles */
-	float q1q1 = q[1] * q[1];
-	float q2q2 = q[2] * q[2];
-	float q3q3 = q[3] * q[3];
-	float q1q2 = q[1] * q[2];
-	float q0q2 = q[0] * q[2];
-	float q0q3 = q[0] * q[3];
-	float q1q3 = q[1] * q[3];
-	float q2q3 = q[2] * q[3];
-	float q0q1 = q[0] * q[1];
-
-	//R
-	r[0*3 + 0] = 1.0f - 2.0f * (q2q2 + q3q3);
-	r[0*3 + 1] = 2.0f * (q1q2 - q0q3);
-	r[0*3 + 2] = 2.0f * (q0q2 + q1q3);
-
-	r[1*3 + 0] = 2.0f * (q1q2 + q0q3);
-	r[1*3 + 1] = 1.0f - 2.0f * (q1q1 + q3q3);
-	r[1*3 + 2] = 2.0f * (q2q3 - q0q1);
-
-	r[2*3 + 0] = 2.0f * (q1q3 - q0q2);
-	r[2*3 + 1] = 2.0f * (q0q1 + q2q3);
-	r[2*3 + 2] = 1.0f - 2.0f * (q1q1 + q2q2);
-
-	//transpose(R)
-	r_transpose[0*3 + 0] = r[0*3 + 0];
-	r_transpose[1*3 + 0] = r[0*3 + 1];
-	r_transpose[2*3 + 0] = r[0*3 + 2];
-
-	r_transpose[0*3 + 1] = r[1*3 + 0];
-	r_transpose[1*3 + 1] = r[1*3 + 0];
-	r_transpose[2*3 + 1] = r[1*3 + 2];
-
-	r_transpose[0*3 + 2] = r[2*3 + 0];
-	r_transpose[1*3 + 2] = r[2*3 + 1];
-	r_transpose[2*3 + 2] = r[2*3 + 2];
-}
-
-void vee_map_3x3(float *mat, float *vec)
-{
-	vec[0] = mat[2*3 + 1];
-	vec[1] = mat[0*3 + 2];
-	vec[2] = mat[1*3 + 0];
-}
-
-void hat_map_3x3(float *vec, float *mat)
-{
-	mat[0*3 + 0] = 0.0f;
-	mat[0*3 + 1] = -vec[2];
-	mat[0*3 + 2] = +vec[1];
-	mat[1*3 + 0] = +vec[2];
-	mat[1*3 + 1] = 0.0f;
-	mat[1*3 + 2] = -vec[0];
-	mat[2*3 + 0] = -vec[1];
-	mat[2*3 + 1] = +vec[0];
-	mat[2*3 + 2] = 0.0f;
-}
-
-void cross_product_3x1(float *vec_a, float *vec_b, float *vec_result)
-{
-	vec_result[0] = vec_a[1]*vec_b[2] - vec_a[2]*vec_b[1];
-	vec_result[1] = vec_a[2]*vec_b[0] - vec_a[0]*vec_b[2];
-	vec_result[2] = vec_a[0]*vec_b[1] - vec_a[1]*vec_b[0];
-}
-
-void norm_3x1(float *vec, float *norm)
-{
-	float sq_sum = vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2];
-	arm_sqrt_f32(sq_sum, norm);
-}
-
-void normalize_3x1(float *vec)
-{
-	float sq_sum = vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2];
-	float norm;
-	arm_sqrt_f32(sq_sum, &norm);
-	vec[0] /= norm;
-	vec[1] /= norm;
-	vec[2] /= norm;
 }
 
 void estimate_uav_dynamics(float *gyro, float *moments, float *m_rot_frame)
