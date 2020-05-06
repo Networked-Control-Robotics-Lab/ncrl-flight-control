c435479b6f0298a93f88d5cf1bcf53577d162bbc
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-05-06 23:03:37 +0800

    add ist8310 read sensor function

--- src/driver/device/ist8310.c
+++ src/driver/device/ist8310.c
@@ -4,8 +4,61 @@
 #include "sw_i2c.h"
 #include "delay.h"
 
+SemaphoreHandle_t ist8310_semphr;
+
+float *mag_ptr;
+
+void ist8130_init(void)
+{
+	ist8310_semphr = xSemaphoreCreateBinary();
+}
+
+void ist8310_semaphore_handler(void)
+{
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	xSemaphoreGiveFromISR(ist8310_semphr, &xHigherPriorityTaskWoken);
+	portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
+}
+
+void ist8310_read_sensor(void)
+{
+	uint8_t buf[6];
+
+	uint16_t mx_unscaled, my_unscaled, mz_unscaled;
+
+	sw_i2c_start();
+	sw_i2c_send_byte((0xE << 1) | 0);
+	//TODO: wait ack
+	sw_i2c_send_byte(0x3);
+	//TODO: wait ack
+	sw_i2c_start();
+
+	buf[0] = sw_i2c_read_byte();
+	//TODO: wait ack
+	buf[1] = sw_i2c_read_byte();
+	//TODO: wait ack
+	buf[2] = sw_i2c_read_byte();
+	//TODO: wait ack
+	buf[3] = sw_i2c_read_byte();
+	//TODO: wait ack
+	buf[4] = sw_i2c_read_byte();
+	//TODO: wait ack
+	buf[5] = sw_i2c_read_byte();
+	//TODO: wait ack
+
+	mx_unscaled = ((uint16_t)buf[1]) << 8 | buf[0];
+	my_unscaled = ((uint16_t)buf[3]) << 8 | buf[2];
+	mz_unscaled = ((uint16_t)buf[5]) << 8 | buf[4];
+
+	mag_ptr[0] = mx_unscaled; //TODO: convert scale
+	mag_ptr[1] = my_unscaled;
+	mag_ptr[2] = mz_unscaled;
+}
+
 void ist8310_task_handler(void)
 {
+	//ist8130_init(); //XXX
+
 	while(1) {
 		sw_i2c_start();
 		sw_i2c_send_byte(0xA0);
--- src/driver/device/ist8310.h
+++ src/driver/device/ist8310.h
@@ -1,6 +1,8 @@
 #ifndef __IST8310_H__
 #define __IST8310_H__
 
+void ist8130_init(void);
+
 void ist8310_task_handler(void);
 
 #endif
