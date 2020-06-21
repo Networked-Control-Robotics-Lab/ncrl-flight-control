ba94feda7856f3895d7e7cc8fc7f0d2ed6c9d571
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-06-21 15:00:49 +0800

    add flash driver

---
+++ src/driver/periph/flash.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+#include "stm32f4xx.h"
+#include "flash.h"
+#include "uart.h"
+
+uint32_t get_flash_sector_num(uint32_t addr)
+{
+	uint32_t sector = 0;
+
+	if((addr < ADDR_FLASH_SECTOR_1) && (addr >= ADDR_FLASH_SECTOR_0)) {
+		sector = FLASH_Sector_0;
+	} else if((addr < ADDR_FLASH_SECTOR_2) && (addr >= ADDR_FLASH_SECTOR_1)) {
+		sector = FLASH_Sector_1;
+	} else if((addr < ADDR_FLASH_SECTOR_3) && (addr >= ADDR_FLASH_SECTOR_2)) {
+		sector = FLASH_Sector_2;
+	} else if((addr < ADDR_FLASH_SECTOR_4) && (addr >= ADDR_FLASH_SECTOR_3)) {
+		sector = FLASH_Sector_3;
+	} else if((addr < ADDR_FLASH_SECTOR_5) && (addr >= ADDR_FLASH_SECTOR_4)) {
+		sector = FLASH_Sector_4;
+	} else if((addr < ADDR_FLASH_SECTOR_6) && (addr >= ADDR_FLASH_SECTOR_5)) {
+		sector = FLASH_Sector_5;
+	} else if((addr < ADDR_FLASH_SECTOR_7) && (addr >= ADDR_FLASH_SECTOR_6)) {
+		sector = FLASH_Sector_6;
+	} else if((addr < ADDR_FLASH_SECTOR_8) && (addr >= ADDR_FLASH_SECTOR_7)) {
+		sector = FLASH_Sector_7;
+	} else if((addr < ADDR_FLASH_SECTOR_9) && (addr >= ADDR_FLASH_SECTOR_8)) {
+		sector = FLASH_Sector_8;
+	} else if((addr < ADDR_FLASH_SECTOR_10) && (addr >= ADDR_FLASH_SECTOR_9)) {
+		sector = FLASH_Sector_9;
+	} else if((addr < ADDR_FLASH_SECTOR_11) && (addr >= ADDR_FLASH_SECTOR_10)) {
+		sector = FLASH_Sector_10;
+	} else { /* (addr < FLASH_END_ADDR) && (addr >= ADDR_FLASH_SECTOR_11) */
+		sector = FLASH_Sector_11;
+	}
+
+	return sector;
+}
+
+int flash_write(uint32_t start_addr, uint32_t *data_arr, int size)
+{
+	FLASH_Unlock();
+
+	uint32_t end_addr = start_addr + size;
+
+	uint32_t start_sector = get_flash_sector_num(start_addr);
+	uint32_t end_sector = get_flash_sector_num(end_addr);
+
+	FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR |
+	                FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
+
+	int i;
+
+	/* erase flash sector */
+	for(i = start_sector; i <= end_sector; i += 8) {
+		//voltage range 3 (2.7V ~ 3.6V) = word writting mode
+		while(FLASH_EraseSector(i, VoltageRange_3) != FLASH_COMPLETE) {
+			//XXX: timeout
+		}
+	}
+
+	/* write data into flash sector */
+	for(i = 0; i < size; i++) {
+		uint32_t write_addr = start_addr + i;
+		while(FLASH_ProgramWord(write_addr, data_arr[i]) != FLASH_COMPLETE) {
+			//XXX: timeout
+		}
+	}
+
+	/* verify the written data */
+	uint32_t data_in_flash;
+	for(i = 0; i < size; i++) {
+		uint32_t read_addr = start_addr + i;
+		data_in_flash = *(uint32_t *)read_addr;
+
+		if(data_in_flash != data_arr[i]) {
+			return FLASH_WR_DATA_INCORRECT;
+		}
+	}
+
+	FLASH_Lock();
+
+	return FLASH_WR_SUCCEED;
+}
+
+void debug_print_flash(uint32_t start_address, int size)
+{
+	uint32_t data_read;
+
+	char s[50] = {0};
+
+	int i;
+	for(i = 0; i < size; i++) {
+		uint32_t read_addr = start_address + i;
+		data_read = *(uint32_t *)read_addr;
+
+		sprintf(s, "%p: %lu\n\r", (uint32_t *)read_addr, data_read);
+		uart3_puts(s, strlen(s));
+	}
+}
---
+++ src/driver/periph/flash.h
@@ -0,0 +1,27 @@
+#ifndef __FLASH_H__
+#define __FLASH_H__
+
+#define ADDR_FLASH_SECTOR_0  ((uint32_t)0x08000000) //base addrress of sector 0, 16KB
+#define ADDR_FLASH_SECTOR_1  ((uint32_t)0x08004000) //base addrress of sector 1, 16KB
+#define ADDR_FLASH_SECTOR_2  ((uint32_t)0x08008000) //base addrress of sector 2, 16KB
+#define ADDR_FLASH_SECTOR_3  ((uint32_t)0x0800C000) //base addrress of sector 3, 16KB
+#define ADDR_FLASH_SECTOR_4  ((uint32_t)0x08010000) //base addrress of sector 4, 64KB
+#define ADDR_FLASH_SECTOR_5  ((uint32_t)0x08020000) //base addrress of sector 5, 128KB
+#define ADDR_FLASH_SECTOR_6  ((uint32_t)0x08040000) //base addrress of sector 6, 128KB
+#define ADDR_FLASH_SECTOR_7  ((uint32_t)0x08060000) //base addrress of sector 7, 128KB
+#define ADDR_FLASH_SECTOR_8  ((uint32_t)0x08080000) //base addrress of sector 8, 128KB
+#define ADDR_FLASH_SECTOR_9  ((uint32_t)0x080A0000) //base addrress of sector 9, 128KB
+#define ADDR_FLASH_SECTOR_10 ((uint32_t)0x080C0000) //base addrress of sector 10, 128KB
+#define ADDR_FLASH_SECTOR_11 ((uint32_t)0x080E0000) //base addrress of sector 11, 128KB
+
+enum {
+	FLASH_WR_SUCCEED = 0,
+	FLASH_WR_DATA_INCORRECT = 1,
+	FLASH_WR_TIMEOUT = 2,
+	FLASH_ERASE_TIMEOUT = 3
+} FLASH_RET_VAL;
+
+int flash_write(uint32_t start_addr, uint32_t *data_arr, int size);
+void debug_print_flash(uint32_t start_address, int size);
+
+#endif
