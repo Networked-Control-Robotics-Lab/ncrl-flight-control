754526167715a12c35e68002bda98ff1cf4919d9
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-06-21 19:31:05 +0800

    driver: add state machine parser for ublox m8n

--- src/driver/device/ublox_m8n.c
+++ src/driver/device/ublox_m8n.c
@@ -1,14 +1,20 @@
 #include "stm32f4xx_conf.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "ublox_m8n.h"
 
 #define UBLOX_M8N_QUEUE_SIZE 100
+
+#define UBX_SYNC_C1 0xb5
+#define UBX_SYNC_C2 0x62
 
 typedef struct {
 	char c;
 } ublox_m8n_buf_c_t;
 
 QueueHandle_t ublox_m8n_queue;
+
+ublox_t ublox;
 
 void ublox_m8n_init(void)
 {
@@ -38,10 +44,81 @@ void ublox_m8n_isr_handler(uint8_t c)
 	portEND_SWITCHING_ISR(higher_priority_task_woken);
 }
 
+void ublox_decode_nav_pvt_msg(void)
+{
+	if((ublox.recept_class != 0x01) || (ublox.recept_id != 0x07) ||
+	    (ublox.recept_len != 92)) {
+		return;
+	}
+
+	uint8_t calc_ck[2];
+	ublox_checksum_calc(calc_ck, ublox.recept_buf, 92);
+	if(calc_ck != ublox.recept_ck) {
+		return;
+	}
+}
+
 void ublox_m8n_gps_update(void)
 {
+	/* ubx message protocol:
+	   +---------+---------+-------+----+-----+----------+-----------+------------+
+	   | sync c1 | sync c2 | class | id | len | payloads | checksum1 | checksum 2 |
+	   +---------+---------+-------+----+-----+----------+-----------+------------+*/
+
 	ublox_m8n_buf_c_t recept_c;
 	while(xQueueReceive(ublox_m8n_queue, &recept_c, 0) == pdTRUE) {
-		//uint8_t c = recept_c.c;
+		uint8_t c = recept_c.c;
+
+		switch(ublox.parse_state) {
+		case UBX_STATE_WAIT_SYNC_C1:
+			if(c == UBX_SYNC_C1) {
+				ublox.parse_state = UBX_STATE_WAIT_SYNC_C2;
+				ublox.recept_buf_ptr = 0;
+			}
+			break;
+		case UBX_STATE_WAIT_SYNC_C2:
+			if(c == UBX_SYNC_C2) {
+				ublox.parse_state = UBX_STATE_RECEIVE_CLASS;
+			} else {
+				ublox.parse_state = UBX_STATE_WAIT_SYNC_C1;
+			}
+			break;
+		case UBX_STATE_RECEIVE_CLASS:
+			ublox.recept_class = c;
+			ublox.parse_state = UBX_STATE_RECEIVE_ID;
+			break;
+		case UBX_STATE_RECEIVE_ID:
+			ublox.recept_id = c;
+			ublox.parse_state = UBX_STATE_RECEIVE_LEN1;
+			break;
+		case UBX_STATE_RECEIVE_LEN1:
+			ublox.recept_len_buf[0] = c;
+			ublox.parse_state = UBX_STATE_RECEIVE_LEN2;
+			break;
+		case UBX_STATE_RECEIVE_LEN2:
+			ublox.recept_len_buf[1] = c;
+			ublox.recept_len = *(uint16_t *)ublox.recept_len_buf;
+			ublox.parse_state = UBX_STATE_RECEIVE_PAYLOAD;
+			break;
+		case UBX_STATE_RECEIVE_PAYLOAD: {
+			if(ublox.recept_buf_ptr == (ublox.recept_len - 1)) {
+				ublox.recept_buf[ublox.recept_buf_ptr] = c;
+				ublox.parse_state = UBX_STATE_RECEIVE_CK1;
+			} else {
+				ublox.recept_buf[ublox.recept_buf_ptr] = c;
+				ublox.recept_buf_ptr++;
+			}
+			break;
+		}
+		case UBX_STATE_RECEIVE_CK1:
+			ublox.recept_ck[0] = c;
+			ublox.parse_state = UBX_STATE_RECEIVE_CK2;
+			break;
+		case UBX_STATE_RECEIVE_CK2:
+			ublox.recept_ck[1] = c;
+			ublox_decode_nav_pvt_msg();
+			ublox.parse_state = UBX_STATE_WAIT_SYNC_C1;
+			break;
+		}
 	}
 }
--- src/driver/device/ublox_m8n.h
+++ src/driver/device/ublox_m8n.h
@@ -1,4 +1,31 @@
 #ifndef __UBLOX_M8N_H__
 #define __UBLOX_M8N_H__
 
+enum {
+	UBX_STATE_WAIT_SYNC_C1 = 0,
+	UBX_STATE_WAIT_SYNC_C2 = 1,
+	UBX_STATE_RECEIVE_CLASS = 2,
+	UBX_STATE_RECEIVE_ID = 3,
+	UBX_STATE_RECEIVE_LEN1 = 4,
+	UBX_STATE_RECEIVE_LEN2 = 5,
+	UBX_STATE_RECEIVE_PAYLOAD = 6,
+	UBX_STATE_RECEIVE_CK1 = 7,
+	UBX_STATE_RECEIVE_CK2 = 8
+} UBX_PARSE_STATE;
+
+typedef struct {
+	int parse_state;
+	uint8_t recept_class;
+	uint8_t recept_id;
+	uint8_t recept_len_buf[2];
+	uint16_t recept_len;
+	uint8_t recept_ck[2];
+	uint8_t recept_buf[100];
+	uint16_t recept_buf_ptr;
+} ublox_t;
+
+void ublox_m8n_init(void);
+void ublox_m8n_isr_handler(uint8_t c);
+void ublox_m8n_gps_update(void);
+
 #endif
