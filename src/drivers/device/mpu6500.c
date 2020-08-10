08e804276ca586896ac534af94cfbc6a6f87e8e5
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-08-10 17:37:03 +0800

    remove redundent code of mpu6500

--- src/drivers/device/mpu6500.c
+++ src/drivers/device/mpu6500.c
@@ -64,23 +64,16 @@ static void mpu6500_bias_calc(int16_t *gyro, int16_t *accel)
 	static int imu_bias_sampling_cnt = IMU_CALIB_SAMPLE_CNT;
 
 	static float _gyro_bias[3] = {0.0f, 0.0f, 0.0f};
-	//static float _accel_bias[3] = {0.0f, 0.0f, 0.0f};
 
 	_gyro_bias[0] += (float)gyro[0] / (float)IMU_CALIB_SAMPLE_CNT;
 	_gyro_bias[1] += (float)gyro[1] / (float)IMU_CALIB_SAMPLE_CNT;
 	_gyro_bias[2] += (float)gyro[2] / (float)IMU_CALIB_SAMPLE_CNT;
-	//_accel_bias[0] += (float)accel[0] / (float)IMU_CALIB_SAMPLE_CNT;
-	//_accel_bias[1] += (float)accel[1] / (float)IMU_CALIB_SAMPLE_CNT;
-	//_accel_bias[2] += (float)accel[2] / (float)IMU_CALIB_SAMPLE_CNT;
 
 	imu_bias_sampling_cnt--;
 	if(imu_bias_sampling_cnt == 0) {
 		mpu6500.gyro_bias[0] = (int16_t)_gyro_bias[0];
 		mpu6500.gyro_bias[1] = (int16_t)_gyro_bias[1];
 		mpu6500.gyro_bias[2] = (int16_t)_gyro_bias[2];
-		//mpu6500.accel_bias[0] = (int16_t)_accel_bias[0];
-		//mpu6500.accel_bias[1] = (int16_t)_accel_bias[1];
-		//mpu6500.accel_bias[2] = 0;  //(int16_t)(_accel_bias[2] - (-9.8f / mpu6500.accel_scale));
 		mpu6500.init_finished = true;
 	}
 }
@@ -106,27 +99,21 @@ void mpu6500_init(imu_t *imu)
 
 	mpu6500_reset();
 
-	//float gravity_full_scale = 0.0f;
-
 	switch(mpu6500.accel_fs) {
 	case MPU6500_GYRO_FS_2G:
 		mpu6500.accel_scale = 9.8 / 16384.0f;
-		//gravity_full_scale = 16384.0f * 2.0f;
 		mpu6500_write_byte(MPU6500_ACCEL_CONFIG, 0x00);
 		break;
 	case MPU6500_GYRO_FS_4G:
 		mpu6500.accel_scale = 9.8f / 8192.0f;
-		//gravity_full_scale = 8192.0f * 2.0f;
 		mpu6500_write_byte(MPU6500_ACCEL_CONFIG, 0x08);
 		break;
 	case MPU6500_GYRO_FS_8G:
 		mpu6500.accel_scale = 9.8f / 4096.0f;
-		//gravity_full_scale = 4096.0f * 2;
 		mpu6500_write_byte(MPU6500_ACCEL_CONFIG, 0x10);
 		break;
 	case MPU6500_GYRO_FS_16G:
 		mpu6500.accel_scale = 9.8f / 2048.0f;
-		//gravity_full_scale = 2048.0f * 2;
 		mpu6500_write_byte(MPU6500_ACCEL_CONFIG, 0x18);
 		break;
 	}
