99cca2f46bad1aef2732b44f84fb5e2e4ba4cd1f
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-08-12 12:26:59 +0800

    rename header macro name

--- src/core/controllers/motor_thrust_fitting.h
+++ src/core/controllers/motor_thrust_fitting.h
@@ -1,5 +1,5 @@
-#ifndef __MOTOR_TRHUST_H__
-#define __MOTOR_THRUST_H__
+#ifndef __MOTOR_TRHUST_FITTING_H__
+#define __MOTOR_THRUST_FITTING_H__
 
 float convert_motor_cmd_to_thrust(float percentage);
 float convert_motor_thrust_to_cmd(float thrust);
