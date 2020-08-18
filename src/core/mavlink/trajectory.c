9e8ba25f3d5d8a2f2bfd33bec5eec90817618020
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-08-18 14:34:56 +0800

    refactor mavlink trajectory micoservice program

--- src/core/mavlink/trajectory.c
+++ src/core/mavlink/trajectory.c
@@ -5,42 +5,27 @@
 #include "sys_time.h"
 #include "optitrack.h"
 #include "localization_system.h"
+#include "trajectory.h"
 
-bool traj_do_ack = false;
-uint8_t traj_ack_val;
-
-bool traj_receiving = false;
-int traj_list_size = 0;
-uint8_t traj_index = 0;
-
-bool z_traj_enabled = false;
-bool yaw_traj_enabled = false;
-
-float recept_start_time = 0.0f;
-bool recept_finished = false;
-
-bool x_received = false;
-bool y_received = false;
-bool z_received = false;
-bool yaw_received = false;
+traj_msg_manager_t traj_msg_manager;
 
 void polynomial_trajectory_microservice_handler(void)
 {
-	if(traj_do_ack == true) {
+	if(traj_msg_manager.do_ack == true) {
 		mavlink_message_t msg;
 		mavlink_msg_polynomial_trajectory_ack_pack_chan(
-		        1, 1, MAVLINK_COMM_1, &msg, 255, 0, traj_ack_val);
+		        1, 1, MAVLINK_COMM_1, &msg, 255, 0, traj_msg_manager.ack_val);
 		send_mavlink_msg_to_uart(&msg);
-		traj_do_ack = false;
+		traj_msg_manager.do_ack = false;
 	}
 
-	if(traj_receiving == true || recept_finished == true) {
+	if(traj_msg_manager.do_recept == true || traj_msg_manager.recept_finished == true) {
 		float current_time = get_sys_time_s();
-		if((current_time - recept_start_time) >= 5.0f) {
-			if(recept_finished == true) {
+		if((current_time - traj_msg_manager.recept_start_time) >= 5.0f) {
+			if(traj_msg_manager.recept_finished == true) {
 				/* succeeded: close transaction after 5 seconds in case
 				 * the ground station didn't received the ack message */
-				recept_finished = false;
+				traj_msg_manager.recept_finished = false;
 			} else {
 				/* timeout: transaction failed! */
 				//reset autopilot manager
@@ -48,40 +33,40 @@ void polynomial_trajectory_microservice_handler(void)
 			}
 
 			//stop receiving trajectory item message
-			traj_receiving = false;
+			traj_msg_manager.do_recept = false;
 
 			/* reset reception flags */
-			z_traj_enabled = false;
-			yaw_traj_enabled = false;
-			x_received = false;
-			y_received = false;
-			z_received = false;
-			yaw_received = false;
-			traj_index = 0;
-			traj_list_size = 0;
+			traj_msg_manager.z_planned = false;
+			traj_msg_manager.yaw_planned = false;
+			traj_msg_manager.x_recvd = false;
+			traj_msg_manager.y_recvd = false;
+			traj_msg_manager.z_recvd = false;
+			traj_msg_manager.yaw_recvd = false;
+			traj_msg_manager.recept_index = 0;
+			traj_msg_manager.list_size = 0;
 		}
 	}
 }
 
 void trigger_polynomial_trajectory_ack_sending(uint8_t ack_val)
 {
-	traj_ack_val = ack_val;
-	traj_do_ack = true;
+	traj_msg_manager.ack_val = ack_val;
+	traj_msg_manager.do_ack = true;
 }
 
 void mav_polynomial_trajectory_write(mavlink_message_t *received_msg)
 {
-	recept_start_time = get_sys_time_s();
+	traj_msg_manager.recept_start_time = get_sys_time_s();
 
 	mavlink_polynomial_trajectory_write_t poly_traj_write;
 	mavlink_msg_polynomial_trajectory_write_decode(received_msg, &poly_traj_write);
 
 	/* setup trajectory configuration of autopilot*/
-	z_traj_enabled = (poly_traj_write.z_enabled == 0 ? false : true);
-	yaw_traj_enabled = (poly_traj_write.yaw_enabled == 0 ? false : true);
-	traj_list_size = poly_traj_write.list_size;
-	int ret_val = autopilot_config_trajectory_following(traj_list_size, z_traj_enabled,
-	                yaw_traj_enabled);
+	traj_msg_manager.z_planned = (poly_traj_write.z_enabled == 0 ? false : true);
+	traj_msg_manager.yaw_planned = (poly_traj_write.yaw_enabled == 0 ? false : true);
+	traj_msg_manager.list_size = poly_traj_write.list_size;
+	int ret_val = autopilot_config_trajectory_following(traj_msg_manager.list_size, traj_msg_manager.z_planned,
+	                traj_msg_manager.yaw_planned);
 
 	switch(ret_val) {
 	case AUTOPILOT_TRAJ_LIST_TOO_LARGE:
@@ -97,13 +82,13 @@ void mav_polynomial_trajectory_write(mavlink_message_t *received_msg)
 		trigger_polynomial_trajectory_ack_sending(TRAJECTORY_ACK_ERROR);
 	}
 
-	x_received = false;
-	y_received = false;
-	z_received = false;
-	yaw_received = false;
-	recept_finished = false;
-	traj_receiving = true;
-	traj_index = 0;
+	traj_msg_manager.x_recvd = false;
+	traj_msg_manager.y_recvd = false;
+	traj_msg_manager.z_recvd = false;
+	traj_msg_manager.yaw_recvd = false;
+	traj_msg_manager.recept_finished = false;
+	traj_msg_manager.do_recept = true;
+	traj_msg_manager.recept_index = 0;
 }
 
 void mav_polynomial_trajectory_cmd(mavlink_message_t *received_msg)
@@ -142,9 +127,9 @@ void mav_polynomial_trajectory_cmd(mavlink_message_t *received_msg)
 void mav_polynomial_trajectory_item(mavlink_message_t *received_msg)
 {
 	/* should not receive any trajectory if handshaking not happened */
-	if(traj_receiving == false) return;
+	if(traj_msg_manager.do_recept == false) return;
 
-	recept_start_time = get_sys_time_s();
+	traj_msg_manager.recept_start_time = get_sys_time_s();
 
 	mavlink_polynomial_trajectory_item_t poly_traj_item;
 	mavlink_msg_polynomial_trajectory_item_decode(received_msg, &poly_traj_item);
@@ -152,7 +137,7 @@ void mav_polynomial_trajectory_item(mavlink_message_t *received_msg)
 	int ret_val = 0;
 
 	/* check current trajectory index to receive */
-	if(poly_traj_item.index != traj_index) {
+	if(poly_traj_item.index != traj_msg_manager.recept_index) {
 		/* index mismatched */
 		trigger_polynomial_trajectory_ack_sending(TRAJECTORY_ACK_ERROR);
 		return;
@@ -161,24 +146,24 @@ void mav_polynomial_trajectory_item(mavlink_message_t *received_msg)
 	/* save trajectory and decide next receiption type */
 	switch(poly_traj_item.type) {
 	case TRAJECTORY_POSITION_X:
-		ret_val = autopilot_set_x_trajectory(traj_index, poly_traj_item.coeff,
+		ret_val = autopilot_set_x_trajectory(traj_msg_manager.recept_index, poly_traj_item.coeff,
 		                                     poly_traj_item.flight_time);
-		x_received = true;
+		traj_msg_manager.x_recvd = true;
 		break;
 	case TRAJECTORY_POSITION_Y:
-		ret_val = autopilot_set_y_trajectory(traj_index, poly_traj_item.coeff,
+		ret_val = autopilot_set_y_trajectory(traj_msg_manager.recept_index, poly_traj_item.coeff,
 		                                     poly_traj_item.flight_time);
-		y_received = true;
+		traj_msg_manager.y_recvd = true;
 		break;
 	case TRAJECTORY_POSITION_Z:
-		ret_val = autopilot_set_z_trajectory(traj_index, poly_traj_item.coeff,
+		ret_val = autopilot_set_z_trajectory(traj_msg_manager.recept_index, poly_traj_item.coeff,
 		                                     poly_traj_item.flight_time);
-		z_received = true;
+		traj_msg_manager.z_recvd = true;
 		break;
 	case TRAJECTORY_ANGLE_YAW:
-		ret_val = autopilot_set_yaw_trajectory(traj_index, poly_traj_item.coeff,
+		ret_val = autopilot_set_yaw_trajectory(traj_msg_manager.recept_index, poly_traj_item.coeff,
 		                                       poly_traj_item.flight_time);
-		yaw_received = true;
+		traj_msg_manager.yaw_recvd = true;
 		break;
 	}
 
@@ -196,19 +181,19 @@ void mav_polynomial_trajectory_item(mavlink_message_t *received_msg)
 		trigger_polynomial_trajectory_ack_sending(TRAJECTORY_ACK_ERROR);
 	}
 
-	if((x_received == true) && (y_received == true) &&
-	    (z_received == true || z_traj_enabled == false) &&
-	    (yaw_received == true || yaw_traj_enabled == false)) {
-		traj_index++; //next trajectory segment to receive
-		x_received = false;
-		y_received = false;
-		z_received = false;
-		yaw_received = false;
+	if((traj_msg_manager.x_recvd == true) && (traj_msg_manager.y_recvd == true) &&
+	    (traj_msg_manager.z_recvd == true || traj_msg_manager.z_planned == false) &&
+	    (traj_msg_manager.yaw_recvd == true || traj_msg_manager.yaw_planned == false)) {
+		traj_msg_manager.recept_index++; //next trajectory segment to receive
+		traj_msg_manager.x_recvd = false;
+		traj_msg_manager.y_recvd = false;
+		traj_msg_manager.z_recvd = false;
+		traj_msg_manager.yaw_recvd = false;
 	}
 
 	/* finish receiving all trajectory segments */
-	if(traj_index >= traj_list_size) {
-		recept_finished = true;
+	if(traj_msg_manager.recept_index >= traj_msg_manager.list_size) {
+		traj_msg_manager.recept_finished = true;
 	}
 }
 
--- src/core/mavlink/trajectory.h
+++ src/core/mavlink/trajectory.h
@@ -3,6 +3,25 @@
 
 #include "mavlink.h"
 
+typedef struct {
+	bool do_ack;
+	uint8_t ack_val;
+
+	bool do_recept;
+	int list_size;
+	uint8_t recept_index;
+	float recept_start_time;
+	bool recept_finished;
+
+	bool z_planned;
+	bool yaw_planned;
+
+	bool x_recvd;
+	bool y_recvd;
+	bool z_recvd;
+	bool yaw_recvd;
+} traj_msg_manager_t;
+
 void mav_polynomial_trajectory_write(mavlink_message_t *received_msg);
 void mav_polynomial_trajectory_cmd(mavlink_message_t *received_msg);
 void mav_polynomial_trajectory_item(mavlink_message_t *received_msg);
