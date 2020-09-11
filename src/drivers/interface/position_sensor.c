428ec78033a346efbf2d769550f8547ba3b196bc
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-09-11 11:20:30 +0800

    add selection options in position_sensor.c

--- src/drivers/interface/position_sensor.c
+++ src/drivers/interface/position_sensor.c
@@ -3,26 +3,60 @@
 
 bool is_xy_position_info_available(void)
 {
+#if (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_OPTITRACK)
 	return optitrack_available();
+#elif (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_GPS)
+	//XXX
+#else
+	return false;
+#endif
 }
 
 bool is_height_info_available(void)
 {
+#if (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_OPTITRACK)
 	return optitrack_available();
+#elif (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_BAROMETER)
+	//XXX
+#else
+	return false;
+#endif
 }
 
 void get_enu_position(float *pos)
 {
+	/* x-y position */
+#if (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_OPTITRACK)
 	optitrack_read_pos_x(&pos[0]);
 	optitrack_read_pos_y(&pos[1]);
+#elif (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_GPS)
+	//XXX
+#else
+	pos[0] = 0.0f;
+	pos[1] = 0.0f;
+#endif
+
+	/* z-position */
+#if (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_OPTITRACK)
 	optitrack_read_pos_z(&pos[2]);
+#elif (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_BAROMETER)
+	//XXX
+#else
+	pos[2] = 0.0f;
+#endif
 }
 
 float get_enu_height(void)
 {
+#if (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_OPTITRACK)
 	float height;
 	optitrack_read_pos_z(&height);
 	return height;
+#elif (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_BAROMETER)
+	//XXX
+#else
+	return 0.0f;
+#endif
 }
 
 void get_wgs84_position(float *latitude, float *longtitude, float *height)
@@ -31,7 +65,23 @@ void get_wgs84_position(float *latitude, float *longtitude, float *height)
 
 void get_enu_velocity(float *vel)
 {
+	/* x-y position */
+#if (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_OPTITRACK)
 	optitrack_read_vel_x(&vel[0]);
 	optitrack_read_vel_y(&vel[1]);
+#elif (SELECT_POSITION_SENSOR == POSITION_SENSOR_USE_GPS)
+	//XXX
+#else
+	vel[0] = 0.0f;
+	vel[1] = 0.0f;
+#endif
+
+	/* z-position */
+#if (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_OPTITRACK)
 	optitrack_read_vel_z(&vel[2]);
+#elif (SELECT_HEIGHT_SENSOR == HEIGHT_SENSOR_USE_BAROMETER)
+	//XXX
+#else
+	vel[2] = 0.0f;
+#endif
 }
