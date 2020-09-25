1eae59afb43f4df443f7fb95070a8e182020e56e
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-09-25 13:22:36 +0800

    gps: implement function to comvert longitude/latitude to enu coordinates

--- src/core/controllers/autopilot.c
+++ src/core/controllers/autopilot.c
@@ -11,15 +11,6 @@
 
 autopilot_t *autopilot_ptr;
 
-void latitude_longitude_to_cartesian(float latitude, float longitude, float *x, float *y)
-{
-	/* haversine formula, the error is ~0.5% (by assuming earth is spherical symmetry),
-	 * can be improved by using vincenty formula if necessary */
-	*x = EARTH_RADIUS * arm_cos_f32(latitude) * arm_cos_f32(longitude);
-	*y = EARTH_RADIUS * arm_cos_f32(latitude) * arm_sin_f32(longitude);
-	//*z = EARTH_RADIUS * arm_sin_f32(latitude);
-}
-
 bool check_motor_lock_condition(bool condition)
 {
 	return condition; //true: lock, false: unlock
---
+++ src/core/estimators/localization/gps_ekf.c
@@ -0,0 +1,61 @@
+#include "arm_math.h"
+
+#define EARTH_RADIUS 6371.0f
+
+float home_longitude = 0.0f;
+float home_latitude = 0.0f;
+
+float home_ecef_x = 0.0f;
+float home_ecef_y = 0.0f;
+
+void set_home_longitude_latitude(float longitude, float latitude)
+{
+	float sin_lambda = arm_sin_f32(longitude);
+	float cos_lambda = arm_cos_f32(longitude);
+	float cos_phi = arm_cos_f32(latitude);
+
+	home_longitude = longitude;
+	home_latitude = latitude;
+
+	home_ecef_x = EARTH_RADIUS * cos_phi * cos_lambda;
+	home_ecef_y = EARTH_RADIUS * cos_phi * sin_lambda;
+	//home_ecef_z = EARTH_RADIUS * sin_phi
+}
+
+void get_home_longitude_latitude(float *longitude, float *latitude)
+{
+	*longitude = home_longitude;
+	*latitude = home_latitude;
+}
+
+void longitude_latitude_to_enu(float longitude, float latitude,
+                               float *x_enu, float *y_enu)
+{
+	float sin_lambda = arm_sin_f32(longitude);
+	float cos_lambda = arm_cos_f32(longitude);
+	float sin_phi = arm_sin_f32(latitude);
+	float cos_phi = arm_cos_f32(latitude);
+
+	/* convert geodatic coordinates to earth center earth fixed frame (ecef) */
+	float ecef_now_x = EARTH_RADIUS * cos_phi * cos_lambda;
+	float ecef_now_y = EARTH_RADIUS * cos_phi * sin_lambda;
+	//float ecef_now_z = EARTH_RADIUS * sin_phi;
+
+	/* convert position from earth center earth fixed frame to east north up frame */
+	float r11 = -sin_lambda;
+	float r12 = -cos_lambda * sin_phi;
+	//float r13 = cos_lambda * cos_phi;
+	float r21 = cos_lambda;
+	float r22 = -sin_lambda * sin_phi;
+	//float r23 = sin_lambda * cos_phi;
+	//float r31 = 0.0f;
+	//float r32 = cos_phi;
+	//float r33 = sin_phi;
+
+	float dx = ecef_now_x - home_ecef_x;
+	float dy = ecef_now_y - home_ecef_y;
+	//float dz = ecef_now_z - home_ecef_z;
+
+	*x_enu = (r11 * dx) + (r12 * dy);
+	*y_enu = (r21 * dx) + (r22 * dy);
+}
