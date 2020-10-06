2740091cb7fb7c5b563e1b54adc593d03d35c39e
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-10-06 13:00:52 +0800

    remove matrix assert

--- src/common/matrix.h
+++ src/common/matrix.h
@@ -16,32 +16,23 @@
 	float mat ## _arr[row * col]; \
 	arm_mat_init_f32(&mat, row, col, (float32_t *)mat ## _arr)
 
-#define MAT_ASSERT(statement) \
-	while(!(statement)) {led_on(LED_R);}
-
 #define MAT_ADD(mat_a, mat_b, mat_result) \
 	mat_op_status = arm_mat_add_f32(mat_a, mat_b, mat_result); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define MAT_SUB(mat_a, mat_b, mat_result) \
 	mat_op_status = arm_mat_sub_f32(mat_a, mat_b, mat_result); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define MAT_MULT(mat_a, mat_b, mat_result) \
 	mat_op_status = arm_mat_mult_f32(mat_a, mat_b, mat_result); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define MAT_SCALE(mat_in, scale, mat_out) \
 	mat_op_status = arm_mat_scale_f32(mat_in, scale, mat_out); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define MAT_TRANS(mat, mat_trans) \
 	mat_op_status = arm_mat_trans_f32(mat, mat_trans); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define MAT_INV(mat, mat_inv) \
 	mat_op_status = arm_mat_inverse_f32(mat, mat_inv); \
-	MAT_ASSERT(mat_op_status == ARM_MATH_SUCCESS);
 
 #define mat_data(mat) mat.pData
 
