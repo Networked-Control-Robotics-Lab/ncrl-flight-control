09ec29712e2c73b1448d1200e92d64658ba6586e
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-12-25 15:36:55 +0800

    implement matrix reset function

--- src/common/matrix.c
+++ src/common/matrix.c
@@ -4,3 +4,13 @@
 #include "matrix.h"
 
 volatile arm_status mat_op_status = 0;
+
+void matrix_reset(float *data, int row_num, int column_num)
+{
+	int r, c;
+	for(r = 0; r < row_num; r++) {
+		for(c = 0; c < column_num; c++) {
+			data[r * column_num + c] = 0;
+		}
+	}
+}
