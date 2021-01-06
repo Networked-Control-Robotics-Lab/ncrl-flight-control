c66a684ba3d0625fd2deee98cece98279858bac2
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-01-06 14:31:50 +0800

    ins: comp_filter: update velocity before position

--- src/core/state_estimator/ins/ins_comp_filter.c
+++ src/core/state_estimator/ins/ins_comp_filter.c
@@ -25,13 +25,13 @@ void ins_comp_filter_init(float _dt)
 	half_dt_squared = (_dt * _dt) / 2.0f;
 
 	/* position fusion weights */
-	pos_a[0] = 0.1f;   //weight of using gps raw x position
-	pos_a[1] = 0.1f;   //weight of using gps raw y position
+	pos_a[0] = 0.35f;   //weight of using gps raw x position
+	pos_a[1] = 0.35f;   //weight of using gps raw y position
 	pos_a[2] = 0.03f; //weight of using barometer height
 
 	/* velocity fusion weights */
-	vel_a[0] = 0.65f;   //weight of using gps raw x velocity
-	vel_a[1] = 0.65f;   //weight of using gps raw y velocity
+	vel_a[0] = 0.8f;   //weight of using gps raw x velocity
+	vel_a[1] = 0.8f;   //weight of using gps raw y velocity
 	vel_a[2] = 0.03f; //weight of using barometer height velocity
 }
 
@@ -70,19 +70,20 @@ void ins_comp_filter_predict(float *pos_enu_out, float *vel_enu_out,
 
 	/* kinematics update with px, py, vx, vy if gps receiver in presence */
 	if(gps_available == true) {
-		pos_enu_out[0] = pos_last[0] + (vel_last[0] * dt) +
-		                 (accel_i_enu[0] * half_dt_squared);
-		pos_enu_out[1] = pos_last[1] + (vel_last[1] * dt) +
-		                 (accel_i_enu[1] * half_dt_squared);
 		vel_enu_out[0] = vel_last[0] + (accel_i_enu[0] * dt);
 		vel_enu_out[1] = vel_last[1] + (accel_i_enu[1] * dt);
+
+		pos_enu_out[0] = pos_last[0] + (vel_enu_out[0] * dt) +
+		                 (accel_i_enu[0] * half_dt_squared);
+		pos_enu_out[1] = pos_last[1] + (vel_enu_out[1] * dt) +
+		                 (accel_i_enu[1] * half_dt_squared);
 	}
 
 	/* kinematics update with pz, vz if complementary height sensor in presence */
 	if(height_available == true) {
-		pos_enu_out[2] = pos_last[2] + (vel_last[2] * dt) +
-		                 (accel_i_enu[2] * half_dt_squared);
 		vel_enu_out[2] = vel_last[2] + (accel_i_enu[2] * dt);
+		pos_enu_out[2] = pos_last[2] + (vel_enu_out[2] * dt) +
+		                 (accel_i_enu[2] * half_dt_squared);
 	}
 
 	/* save predicted result */
