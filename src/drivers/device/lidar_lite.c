e261a1fba171cfd7d6af9466e3c05135d4c4be4a
Shengwen Cheng <shengwen1997.tw@gmail.com>
2021-06-20 21:04:22 +0800

    lidar_lite: fix i2c read procedure

--- src/drivers/device/lidar_lite.c
+++ src/drivers/device/lidar_lite.c
@@ -13,6 +13,7 @@ void lidar_blocked_read_byte(uint8_t addr, uint8_t *data)
 	sw_i2c_blocked_wait_ack();
 	sw_i2c_blocked_send_byte(addr);
 	sw_i2c_blocked_wait_ack();
+	sw_i2c_blocked_stop();
 	sw_i2c_blocked_start();
 	sw_i2c_blocked_send_byte((LIDAR_DEV_ADDRESS << 1) | 1);
 	sw_i2c_blocked_wait_ack();
@@ -28,6 +29,7 @@ void lidar_read_byte(uint8_t addr, uint8_t *data)
 	sw_i2c_wait_ack();
 	sw_i2c_send_byte(addr);
 	sw_i2c_wait_ack();
+	sw_i2c_stop();
 	sw_i2c_start();
 	sw_i2c_send_byte((LIDAR_DEV_ADDRESS << 1) | 1);
 	sw_i2c_wait_ack();
@@ -36,13 +38,14 @@ void lidar_read_byte(uint8_t addr, uint8_t *data)
 	sw_i2c_stop();
 }
 
-void lidar_blocked_read_bytes(uint8_t addr, uint16_t *data, int size)
+void lidar_blocked_read_bytes(uint8_t addr, uint8_t *data, int size)
 {
 	sw_i2c_blocked_start();
 	sw_i2c_blocked_send_byte((LIDAR_DEV_ADDRESS << 1) | 0);
 	sw_i2c_blocked_wait_ack();
 	sw_i2c_blocked_send_byte(addr);
 	sw_i2c_blocked_wait_ack();
+	sw_i2c_blocked_stop();
 	sw_i2c_blocked_start();
 	sw_i2c_blocked_send_byte((LIDAR_DEV_ADDRESS << 1) | 1);
 	sw_i2c_blocked_wait_ack();
@@ -59,13 +62,14 @@ void lidar_blocked_read_bytes(uint8_t addr, uint16_t *data, int size)
 	sw_i2c_blocked_stop();
 }
 
-void lidar_read_bytes(uint8_t addr, uint16_t *data, int size)
+void lidar_read_bytes(uint8_t addr, uint8_t *data, int size)
 {
 	sw_i2c_start();
 	sw_i2c_send_byte((LIDAR_DEV_ADDRESS << 1) | 0);
 	sw_i2c_wait_ack();
 	sw_i2c_send_byte(addr);
 	sw_i2c_wait_ack();
+	sw_i2c_stop();
 	sw_i2c_start();
 	sw_i2c_send_byte((LIDAR_DEV_ADDRESS << 1) | 1);
 	sw_i2c_wait_ack();
@@ -109,38 +113,30 @@ void lidar_write_byte(uint8_t addr, uint8_t data)
 void lidar_lite_init(void)
 {
 	/* reset lidar */
-	lidar_blocked_write_byte(LIDAR_ACQ_COMMAND_REG, 0x00);
+	lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x00);
 	freertos_task_delay(1000);
 
 	/* continuous reading mode */
-	lidar_blocked_write_byte(LIDAR_MEASURE_COUNT_REG, 0xff);
+	lidar_write_byte(LIDAR_MEASURE_COUNT_REG, 0xff);
 	freertos_task_delay(10);
 
 	/* measurement rate */
-	lidar_blocked_write_byte(LIDAR_MEASURE_DELAY_REG, 0x02);
+	lidar_write_byte(LIDAR_MEASURE_DELAY_REG, 0x02);
 	freertos_task_delay(10);
 
 	/* fast reading mode */
-	lidar_blocked_write_byte(LIDAR_ACQ_CONFIG_REG, 0x21);
+	lidar_write_byte(LIDAR_ACQ_CONFIG_REG, 0x21);
 	freertos_task_delay(10);
 
 	/* start distance measurement */
-	lidar_blocked_write_byte(LIDAR_ACQ_COMMAND_REG, 0x04);
+	lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x04);
 	freertos_task_delay(10);
-}
-
-void lidar_lite_isr_handler(void)
-{
-	lidar_lite.measurement_ready = true;
 }
 
 void lidar_lite_task_handler(void)
 {
-	if(lidar_lite.measurement_ready == true) {
-		lidar_read_bytes(LIDAR_DISTANCE_REG, &lidar_lite.dist_raw, 2);
-		lidar_read_byte(LIDAR_VELOCITY_REG, &lidar_lite.vel_raw);
-		lidar_lite.measurement_ready = false;
-	}
+	lidar_read_bytes(LIDAR_DISTANCE_REG, (uint8_t *)&lidar_lite.dist_raw, 2);
+//	lidar_read_byte(LIDAR_VELOCITY_REG, &lidar_lite.vel_raw);
 }
 
 float lidar_lite_get_distance(void)
--- src/drivers/device/lidar_lite.h
+++ src/drivers/device/lidar_lite.h
@@ -17,13 +17,10 @@
 typedef struct {
 	uint16_t dist_raw; //[cm]
 	uint8_t vel_raw;   //[cm/s]
-
-	bool measurement_ready;
 } lidar_lite_t;
 
 void lidar_lite_init(void);
 
-void lidar_lite_isr_handler(void);
 void lidar_lite_task_handler(void);
 
 float lidar_lite_get_distance(void);
