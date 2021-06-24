455772c96c0bc81adf7f6a02e244dd43075329b9
Shengwen Cheng <shengwen1997.tw@gmail.com>
2021-06-24 17:50:12 +0800

    median_filter: fix bubble sort and median value selection

--- src/core/filters/median_filter.c
+++ src/core/filters/median_filter.c
@@ -4,17 +4,16 @@ float median_filter(float *measurements, size_t size)
 {
 	/* bubble sort */
 	int i, j;
-	for(i = 0; i < size; i++) {
-		for(j = 0; j < size - 1 - i; j++) {
-			if(measurements[j] < measurements[j + 1]) {
-				float tmp;
-				tmp = measurements[j];
-				measurements[j] = measurements[j + 1];
-				measurements[j + 1] = tmp;
+	for (i = 0 ; i < size - 1; i++) {
+		for (j = 0 ; j < size - i - 1; j++) {
+			if (measurements[j] > measurements[j+1]) {
+				float tmp = measurements[j];
+				measurements[j] = measurements[j+1];
+				measurements[j+1] = tmp;
 			}
 		}
 	}
 
 	/* pick the median value */
-	return (measurements[size / 2] + measurements[size / 2 - 1]) / 2;
+	return ((measurements[size / 2] + measurements[size / 2 - 1])) / 2;
 }
