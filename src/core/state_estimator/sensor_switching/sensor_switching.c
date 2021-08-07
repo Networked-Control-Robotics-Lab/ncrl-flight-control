99506c1021ea8012ad41c98bd3cbb3a0d4e40d61
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-08-07 19:29:10 +0800

    test sensor switching

--- src/core/state_estimator/sensor_switching/sensor_switching.c
+++ src/core/state_estimator/sensor_switching/sensor_switching.c
@@ -10,8 +10,9 @@
 #include "compass.h"
 #include "proj_config.h"
 #include "system_state.h"
+#include "optitrack.h"
 
-#define ENABLE_SENSOR_SWITCHING 0
+#define ENABLE_SENSOR_SWITCHING 1
 
 nav_sys_manager_t nav_sys_manager;
 
@@ -23,10 +24,11 @@ void navigation_system_manager_init(void)
 
 #if 1   /* initial start with the gnss/ins */
 	nav_sys_manager.curr_nav_sys = NAV_GNSS_INS;
-	change_heading_sensor_src(HEADING_FUSION_USE_COMPASS);
-	change_position_sensor_src(POSITION_FUSION_USE_GPS);
-	change_height_sensor_src(HEIGHT_FUSION_USE_RANGEFINDER);
+	//change_heading_sensor_src(HEADING_FUSION_USE_COMPASS);
+	//change_position_sensor_src(POSITION_FUSION_USE_GPS);
+	//change_height_sensor_src(HEIGHT_FUSION_USE_RANGEFINDER);
 #else   /* initial start with the vio */
+	nav_sys_manager.curr_nav_sys = NAV_LOCAL_VIO;
 	change_heading_sensor_src(HEADING_FUSION_USE_VINS_MONO);
 	change_position_sensor_src(POSITION_FUSION_USE_VINS_MONO);
 	change_height_sensor_src(HEIGHT_FUSION_USE_VINS_MONO);
@@ -35,41 +37,49 @@ void navigation_system_manager_init(void)
 
 static void nav_switch_gnss_ins_to_aligned_vio(void)
 {
-	/* curr_nav_sys new navigation system */
 	nav_sys_manager.curr_nav_sys = NAV_GNSS_ALIGNED_VIO;
+
+	/*=====================*
+	 * vio frame alignment *
+	 *=====================*/
+
+	vio_calc_frame_alignment_transform();
+	vio_enable_frame_alignment();
 
 	/*=========================*
 	 * update reference signal *
 	 *=========================*/
 
-	vio_calc_frame_alignment_transform();
-	vio_enable_frame_alignment();
+	change_heading_sensor_src(HEADING_FUSION_USE_VINS_MONO);
+	change_position_sensor_src(POSITION_FUSION_USE_VINS_MONO);
+	change_height_sensor_src(HEIGHT_FUSION_USE_VINS_MONO);
 
 	/*=========================*
 	 * update control setpoint *
 	 *=========================*/
 
 	/* position state from the gnss aligned vio */
-	float p_aligned_vio[3];
-	vio_get_quaternion(p_aligned_vio);
+	float p_global_vio[3];
+	vio_get_position(p_global_vio);
 
 	/* position state from the gnss ins */
 	float p_gnss_ins[3];
-	ins_get_fused_position(p_gnss_ins);
+	//ins_get_fused_position(p_gnss_ins);
+	optitrack_read_pos(p_gnss_ins); //XXX: indoor test only
 
 	/* calculate translation */
-	float p_transform[3];
-	p_transform[0] = p_aligned_vio[0] - p_gnss_ins[0];
-	p_transform[1] = p_aligned_vio[1] - p_gnss_ins[1];
-	p_transform[2] = p_aligned_vio[2] - p_gnss_ins[2];
+	float p_translation[3];
+	p_translation[0] = p_global_vio[0] - p_gnss_ins[0];
+	p_translation[1] = p_global_vio[1] - p_gnss_ins[1];
+	p_translation[2] = p_global_vio[2] - p_gnss_ins[2];
 
 	float xd[3];
 	autopilot_get_pos_setpoint(xd);
 
 	/* calculate coordinate transform */
-	xd[0] += p_transform[0];
-	xd[1] += p_transform[1];
-	xd[2] += p_transform[2];
+	xd[0] += p_translation[0];
+	xd[1] += p_translation[1];
+	xd[2] += p_translation[2];
 
 	/* apply coordinate transform to the controller */
 	autopilot_assign_pos_target(xd[0], xd[1], xd[2]);
@@ -77,7 +87,6 @@ static void nav_switch_gnss_ins_to_aligned_vio(void)
 
 static void nav_switch_non_aligned_vio_to_gnss_ins(void)
 {
-	/* curr_nav_sys new navigation system */
 	nav_sys_manager.curr_nav_sys = NAV_GNSS_INS;
 
 	/*=========================*
@@ -93,23 +102,24 @@ static void nav_switch_non_aligned_vio_to_gnss_ins(void)
 
 	/* position and quaternion state from the gnss ins */
 	float p_gnss_ins[3], q_gnss_ins[4];
-	ins_get_fused_position(p_gnss_ins);
+	//ins_get_fused_position(p_gnss_ins);
+	optitrack_read_pos(p_gnss_ins); //XXX: indoor test only
 	ins_ahrs_get_attitude_quaternion(q_gnss_ins);
 
 	/* position and quaternion state from the nonaligned vio */
-	float p_nonaligned_vio[3], q_nonaligned_vio[4];
-	vio_get_position(p_nonaligned_vio);
-	vio_get_quaternion(q_nonaligned_vio);
+	float p_local_vio[3], q_local_vio[4];
+	vio_get_position(p_local_vio);
+	vio_get_quaternion(q_local_vio);
 
 	/* calculate yaw angle from the quaternion */
 	float yaw_gnss_ins = calc_yaw_from_quat(q_gnss_ins);
-	float yaw_nonaligned_vio = calc_yaw_from_quat(q_nonaligned_vio);
+	float yaw_nonaligned_vio = calc_yaw_from_quat(q_local_vio);
 
 	/* calculate translation */
-	float p_transform[3], yaw_transform;
-	p_transform[0] = p_gnss_ins[0] - p_nonaligned_vio[0];
-	p_transform[1] = p_gnss_ins[1] - p_nonaligned_vio[1];
-	p_transform[2] = p_gnss_ins[2] - p_nonaligned_vio[2];
+	float p_translation[3], yaw_transform;
+	p_translation[0] = p_gnss_ins[0] - p_local_vio[0];
+	p_translation[1] = p_gnss_ins[1] - p_local_vio[1];
+	p_translation[2] = p_gnss_ins[2] - p_local_vio[2];
 	yaw_transform = yaw_gnss_ins - yaw_nonaligned_vio;
 
 	float xd[3], yaw_d;
@@ -117,9 +127,9 @@ static void nav_switch_non_aligned_vio_to_gnss_ins(void)
 	yaw_d = autopilot_get_heading_setpoint();
 
 	/* calculate coordinate transform */
-	xd[0] += p_transform[0];
-	xd[1] += p_transform[1];
-	xd[2] += p_transform[2];
+	xd[0] += p_translation[0];
+	xd[1] += p_translation[1];
+	xd[2] += p_translation[2];
 	yaw_d += yaw_transform;
 
 	/* bound the yaw angle in the range of [-180, +180] */
@@ -136,41 +146,45 @@ static void nav_switch_non_aligned_vio_to_gnss_ins(void)
 
 static void nav_switch_aligned_vio_to_gnss_ins(void)
 {
-	/* curr_nav_sys new navigation system */
 	nav_sys_manager.curr_nav_sys = NAV_GNSS_INS;
 
 	/*=========================*
 	 * update reference signal *
 	 *=========================*/
-	change_heading_sensor_src(HEADING_FUSION_USE_COMPASS);
-	change_position_sensor_src(POSITION_FUSION_USE_GPS);
-	change_height_sensor_src(HEIGHT_FUSION_USE_RANGEFINDER);
+
+	//change_heading_sensor_src(HEADING_FUSION_USE_COMPASS);
+	//change_position_sensor_src(POSITION_FUSION_USE_GPS);
+	//change_height_sensor_src(HEIGHT_FUSION_USE_RANGEFINDER);
+	change_heading_sensor_src(HEADING_FUSION_USE_OPTITRACK);
+	change_position_sensor_src(POSITION_FUSION_USE_OPTITRACK);
+	change_height_sensor_src(HEIGHT_FUSION_USE_OPTITRACK);
 
 	/*=========================*
 	 * update control setpoint *
 	 *=========================*/
 
 	/* position state from the gnss aligned vio */
-	float p_aligned_vio[3];
-	vio_get_position(p_aligned_vio);
+	float p_global_vio[3];
+	vio_get_position(p_global_vio);
 
 	/* position state from the gnss ins */
 	float p_gnss_ins[3];
-	ins_get_fused_position(p_gnss_ins);
+	//ins_get_fused_position(p_gnss_ins);
+	optitrack_read_pos(p_gnss_ins); //XXX: indoor test only
 
 	/* calculate translation */
-	float p_transform[3];
-	p_transform[0] = p_gnss_ins[0] - p_aligned_vio[0];
-	p_transform[1] = p_gnss_ins[1] - p_aligned_vio[1];
-	p_transform[2] = p_gnss_ins[2] - p_aligned_vio[2];
+	float p_translation[3];
+	p_translation[0] = p_gnss_ins[0] - p_global_vio[0];
+	p_translation[1] = p_gnss_ins[1] - p_global_vio[1];
+	p_translation[2] = p_gnss_ins[2] - p_global_vio[2];
 
 	float xd[3];
 	autopilot_get_pos_setpoint(xd);
 
 	/* calculate coordinate transform */
-	xd[0] += p_transform[0];
-	xd[1] += p_transform[1];
-	xd[2] += p_transform[2];
+	xd[0] += p_translation[0];
+	xd[1] += p_translation[1];
+	xd[2] += p_translation[2];
 
 	/* apply coordinate transform to the controller */
 	autopilot_assign_pos_target(xd[0], xd[1], xd[2]);
@@ -199,10 +213,9 @@ void sensor_switching_handler(void)
 
 	/* TODO: gnss/ins failure recovery */
 
-	bool gps_ready = is_gps_available();
-	bool compass_ready = is_compass_available();
+	bool gnss_ins_ready = true;//is_gps_available() && is_compass_available(); //XXX
 	bool vio_ready = vins_mono_available();
-	bool sensors_all_ready = gps_ready && compass_ready && vio_ready;
+	bool sensors_all_ready = gnss_ins_ready && vio_ready;
 
 	if(nav_sys_manager.require_update == false && sensors_all_ready == true) {
 		return;
