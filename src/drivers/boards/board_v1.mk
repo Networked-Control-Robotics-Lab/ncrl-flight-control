8aa62d64809a2e3db1c9a3f8a1a3ece99735efd1
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-05 13:54:40 +0800

    board_support: compile different startup program according to the boards

--- src/drivers/boards/board_v1.mk
+++ src/drivers/boards/board_v1.mk
@@ -7,7 +7,8 @@ SRC+=$(PROJ_ROOT)/drivers/periph/board_v1/uart.c \
 	$(PROJ_ROOT)/drivers/periph/board_v1/pwm.c \
 	$(PROJ_ROOT)/drivers/periph/board_v1/exti.c \
 	$(PROJ_ROOT)/drivers/periph/board_v1/gpio.c \
-	$(PROJ_ROOT)/drivers/boards/board_v1/board_support.c
+	$(PROJ_ROOT)/drivers/boards/board_v1/board_support.c \
+	$(PROJ_ROOT)/drivers/boards/board_v1/system_stm32f4xx.c
 
 CFLAGS+=-I$(PROJ_ROOT)/drivers/periph/board_v1
 CFLAGS+=-I$(PROJ_ROOT)/drivers/boards/board_v1
--- src/drivers/boards/px4.mk
+++ src/drivers/boards/px4.mk
@@ -9,7 +9,8 @@ SRC+=$(PROJ_ROOT)/drivers/periph/px4/uart.c \
 	$(PROJ_ROOT)/drivers/periph/px4/gpio.c \
 	$(PROJ_ROOT)/drivers/periph/px4/i2c.c \
 	$(PROJ_ROOT)/drivers/device/ncp5623c.c \
-	$(PROJ_ROOT)/drivers/boards/px4/board_support.c
+	$(PROJ_ROOT)/drivers/boards/px4/board_support.c \
+	$(PROJ_ROOT)/drivers/boards/px4/system_stm32f4xx.c
 
 CFLAGS+=-I$(PROJ_ROOT)/drivers/periph/px4
 CFLAGS+=-I$(PROJ_ROOT)/drivers/boards/px4
