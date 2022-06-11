a39032bf33df66f029b50df398de788918d3dd6c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-11 12:35:10 +0800

    debug_link_task: add new message in the task

--- src/core/tasks/debug_link_task.c
+++ src/core/tasks/debug_link_task.c
@@ -32,6 +32,7 @@ void task_debug_link(void *param)
 	while(1) {
 		//while(xSemaphoreTake(debug_link_task_semphr, portMAX_DELAY) != pdTRUE);
 		//send_imu_debug_message(&payload);
+		//send_compass_debug_message(&payload);
 		send_attitude_euler_debug_message(&payload);
 		//send_attitude_quaternion_debug_message(&payload);
 		//send_attitude_imu_debug_message(&payload);
