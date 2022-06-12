864e351956d222cd7c1fdda2d7b977b688cab9a4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-12 15:27:08 +0800

    lidar lite: handle i2c write failure in initialization

--- src/drivers/device/lidar_lite.c
+++ src/drivers/device/lidar_lite.c
@@ -140,42 +140,49 @@ int lidar_read_bytes(uint8_t addr, uint8_t *data, int size)
 	return 0;
 }
 
-int lidar_blocked_write_byte(uint8_t addr, uint8_t data)
+void lidar_blocked_write_byte(uint8_t addr, uint8_t data)
 {
 	sw_i2c_blocked_start();
 	sw_i2c_blocked_send_byte((LIDAR_DEV_ADDRESS << 1) | 0);
-
-	if(sw_i2c_blocked_wait_ack()) {
-		/* error: failed to receive acknowledgement */
-		sw_i2c_stop();
-		return 1;
-	}
-
+	sw_i2c_blocked_wait_ack();
 	sw_i2c_blocked_send_byte(addr);
-
-	if(sw_i2c_blocked_wait_ack()) {
-		/* error: failed to receive acknowledgement */
-		sw_i2c_stop();
-		return 1;
-	}
-
+	sw_i2c_blocked_wait_ack();
 	sw_i2c_blocked_send_byte(data);
 	sw_i2c_blocked_wait_ack();
 	sw_i2c_blocked_stop();
-
-	return 0;
 }
 
-void lidar_write_byte(uint8_t addr, uint8_t data)
+int lidar_write_byte(uint8_t addr, uint8_t data)
 {
 	sw_i2c_start();
 	sw_i2c_send_byte((LIDAR_DEV_ADDRESS << 1) | 0);
-	sw_i2c_wait_ack();
+
+	if(sw_i2c_blocked_wait_ack()) {
+		/* error: failed to receive acknowledgement */
+		sw_i2c_stop();
+		return 1;
+	}
+
 	sw_i2c_send_byte(addr);
-	sw_i2c_wait_ack();
+
+	if(sw_i2c_blocked_wait_ack()) {
+		/* error: failed to receive acknowledgement */
+		sw_i2c_stop();
+		return 1;
+	}
+
 	sw_i2c_send_byte(data);
-	sw_i2c_wait_ack();
+
+
+	if(sw_i2c_blocked_wait_ack()) {
+		/* error: failed to receive acknowledgement */
+		sw_i2c_stop();
+		return 1;
+	}
+
 	sw_i2c_stop();
+
+	return 0;
 }
 
 void lidar_lite_init(void)
@@ -186,23 +193,23 @@ void lidar_lite_init(void)
 	lidar_lite.dt *= lidar_lite.prescaler;
 
 	/* reset lidar */
-	lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x00);
+	while(lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x00) != 0);
 	freertos_task_delay(100);
 
 	/* continuous reading mode */
-	lidar_write_byte(LIDAR_MEASURE_COUNT_REG, 0xff);
+	while(lidar_write_byte(LIDAR_MEASURE_COUNT_REG, 0xff) != 0);
 	freertos_task_delay(10);
 
 	/* measurement rate */
-	lidar_write_byte(LIDAR_MEASURE_DELAY_REG, 0x02);
+	while(lidar_write_byte(LIDAR_MEASURE_DELAY_REG, 0x02) != 0);
 	freertos_task_delay(10);
 
 	/* fast reading mode */
-	lidar_write_byte(LIDAR_ACQ_CONFIG_REG, 0x21);
+	while(lidar_write_byte(LIDAR_ACQ_CONFIG_REG, 0x21) != 0);
 	freertos_task_delay(10);
 
 	/* start distance measurement */
-	lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x04);
+	while(lidar_write_byte(LIDAR_ACQ_COMMAND_REG, 0x04) != 0);
 	freertos_task_delay(10);
 }
 
