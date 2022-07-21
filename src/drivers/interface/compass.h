ad354c79613dbb9d501bd291445103416ef64721
EasonHuang-tw <eason27271563.me05@nctu.edu.tw>
2022-07-21 21:02:00 +0800

    add ist8310 unscaled debug link

--- src/drivers/interface/compass.h
+++ src/drivers/interface/compass.h
@@ -7,11 +7,13 @@ bool is_compass_available(void);
 void compass_wait_until_stable(void);
 void get_compass_raw(float *mag);
 void get_compass_lpf(float *mag);
+void get_compass_unscaled(float *mag);
 float get_compass_raw_strength(void);
 float get_compass_lpf_strength(void);
 float get_compass_update_rate(void);
 
 void send_compass_debug_message(debug_msg_t *payload);
+void send_compass_unscaled_debug_message(debug_msg_t *payload);
 
 #endif
 
